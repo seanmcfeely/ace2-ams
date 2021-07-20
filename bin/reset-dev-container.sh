accba43de1e266dc6844aa2e384de5927bbff4b4
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-07-20 10:46:00 -0400

    Uses a single docker-compose file

--- bin/reset-dev-container.sh
+++ bin/reset-dev-container.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 
-# Destroy the existing development environment
-docker-compose -f docker-compose-dev.yml down -v
+# Set the environment variables for the containers. For now these are written to a file in your home directory
+# and then loaded when starting/restarting the dev containers.
+ACE2_ENV_PATH="$HOME/.ace2.env"
+if [ ! -f "$ACE2_ENV_PATH" ]; then
+POSTGRES_DB=ace
+POSTGRES_USER=ace
+POSTGRES_PASSWORD=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
-# Initialize the new development environment
-bin/initialize-dev-environment.py
+cat > "$ACE2_ENV_PATH" <<- EOF
+ACE_DEV=true
+POSTGRES_DB=$POSTGRES_DB
+POSTGRES_USER=$POSTGRES_USER
+POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+DATABASE_URL=postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@db:5432/$POSTGRES_DB
+EOF
+fi
+
+# Load the environment variables for the dev containers
+set -a
+source "$ACE2_ENV_PATH"
+set +a
+
+# Destroy the existing development environment
+docker-compose down -v
 
 # Build the new development environment
-docker-compose -f docker-compose-dev.yml build
+docker-compose build
 
 # Start the development environment
-docker-compose -f docker-compose-dev.yml up -d
+docker-compose up -d
--- bin/test-backend.sh
+++ bin/test-backend.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+# Load the environment variables for the dev containers
+ACE2_ENV_PATH="$HOME/.ace2.env"
+set -a
+source "$ACE2_ENV_PATH"
+set +a
+
 # Remove the leading backend/app/ from the command line argument so the path works inside of the container.
 new_path=${1#backend/app/}
 
-docker-compose -f docker-compose-dev.yml run -e TESTING=1 backend pytest "$new_path" -vv
+docker-compose run -e TESTING=1 backend pytest "$new_path" -vv
