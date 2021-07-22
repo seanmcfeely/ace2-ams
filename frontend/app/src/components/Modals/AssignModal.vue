c04f0a86bd039a96433b514acb1cf1a2c47137f9
hollyfoxx <31867815+hollyfoxx@users.noreply.github.com>
2021-07-22 19:02:18 -0400

    Update AssignModal component to have loading/error messages

--- frontend/app/src/components/Modals/AssignModal.vue
+++ frontend/app/src/components/Modals/AssignModal.vue
@@ -3,14 +3,25 @@
 
 <template>
   <BaseModal :name="this.name" header="Assign Ownership">
+    <div>
+      <div class="p-col"
+           v-if="error">
+        <Message severity="error" @close="handleError">{{error}}</Message>
+      </div>
+    </div>
     <div class="p-m-1 p-grid p-fluid p-formgrid p-grid">
-      <div class="p-field p-col">
+      <div class="p-field p-col"
+           v-if="!isLoading">
         <Dropdown
           v-model="selectedUser"
           :options="users"
-          optionLabel="display_name"
+          optionLabel="displayName"
           placeholder="Select a user"
         />
+      </div>
+      <div class="p-col p-offset-3"
+           v-if="isLoading">
+        <ProgressSpinner/>
       </div>
     </div>
     <template #footer>
@@ -26,36 +37,62 @@
 </template>
 
 <script>
-import user from "../../../services/api/user"
+import { mapState } from 'vuex';
 
 import Button from "primevue/button";
 import Dropdown from "primevue/dropdown";
+import Message from 'primevue/message';
+import ProgressSpinner from 'primevue/progressspinner';
 
 import BaseModal from "./BaseModal";
 
 export default {
   name: "AssignModal",
-  components: { BaseModal, Button, Dropdown},
+  components: { BaseModal, Button, Dropdown, Message, ProgressSpinner},
 
   computed: {
     name() {
       return this.$options.name;
     },
+    isOpen() {
+      return this.$store.getters["modals/allOpen"].includes(this.name);
+    },
+    ...mapState({users: state => state.users.users}),
   },
 
   data() {
     return {
-      selectedUser: null,
-      users: [],
+      error: null,
+      isLoading: false,
+      selectedUser: null
     };
   },
 
-  async created() {
-    this.users = await user.getAllUsers();
-    console.log(this.users);
+  created() {
+    this.loadUsers();
+  },
+
+  watch: {
+    isOpen: function(oldValue, newValue) {
+      if (newValue === false) {
+        this.loadUsers();
+      }
+    }
   },
 
   methods: {
+    async loadUsers() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('users/getAllUsers');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong!';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
+    },
     close() {
       this.selectedUser = null;
       this.$store.dispatch("modals/close", this.name);
