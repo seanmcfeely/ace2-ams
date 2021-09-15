fbd68c870f60c7ba8e3b6c308170bc522b4a1afd
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-09-15 09:23:47 -0400

    Adds tests for JWT authentication

--- backend/app/tests/api/test_auth.py
+++ backend/app/tests/api/test_auth.py
@@ -2,37 +2,31 @@ import pytest
 
 from fastapi import status
 
+from tests.helpers import create_test_user
+from main import app
+
+
+#
+# INVALID TESTS
+
 
 @pytest.mark.parametrize(
-    "username,password,expected_status_code",
+    "username,password",
     [
-        ("johndoe", "abcd1234", status.HTTP_200_OK),
-        ("johndoe", "wrongpassword", status.HTTP_401_UNAUTHORIZED),
-        ("wronguser", "abcd1234", status.HTTP_401_UNAUTHORIZED),
+        ("johndoe", "wrongpassword"),
+        ("wronguser", "abcd1234"),
     ],
 )
-def test_auth(client, username, password, expected_status_code):
-    # Create an alert queue
-    client.post("/api/alert/queue/", json={"value": "test_queue"})
-
-    # Create a user role
-    client.post("/api/user/role/", json={"value": "test_role"})
-
-    # Create a user
-    create_json = {
-        "default_alert_queue": "test_queue",
-        "display_name": "John Doe",
-        "email": "john@test.com",
-        "password": "abcd1234",
-        "roles": ["test_role"],
-        "username": "johndoe",
-    }
-    create = client.post("/api/user/", json=create_json)
-    assert create.status_code == status.HTTP_201_CREATED
+def test_auth_invalid(client, db, username, password):
+    create_test_user(db, "johndoe", "abcd1234")
 
     # Attempt to authenticate
-    auth = client.post("/api/auth/", json={"username": username, "password": password})
-    assert auth.status_code == expected_status_code
+    auth = client.post("/api/auth/", data={"username": username, "password": password})
+    assert auth.status_code == status.HTTP_401_UNAUTHORIZED
+
+    # Attempt to use a bogus token to access a protected API endpoint
+    get = client.get("/api/user/", headers={"Authorization": "Bearer asdf"})
+    assert get.status_code == status.HTTP_401_UNAUTHORIZED
 
 
 @pytest.mark.parametrize(
@@ -47,3 +41,55 @@ def test_missing_required_fields(client, key):
     del create_json[key]
     create = client.post("/api/auth/", json=create_json)
     assert create.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.parametrize(
+    "route",
+    [
+        route for route in app.routes if route.path.startswith("/api/")
+    ],
+)
+def test_invalid_token(client, route):
+    """
+    This tests every registered API endpoint to ensure that it requires token authentication.
+    """
+
+    # There are some special endpoints that do not require authentication.
+    if route.path in ["/api/ping", "/api/auth/"]:
+        return
+
+    for method in route.methods:
+        if method == "POST":
+            client_method = client.post
+        elif method == "GET":
+            client_method = client.get
+        elif method == "PATCH":
+            client_method = client.patch
+        elif method == "DELETE":
+            client_method = client.delete
+        else:
+            raise ValueError(f"Test does not account for method: {method}")
+
+        result = client_method(route.path)
+        assert result.status_code == status.HTTP_401_UNAUTHORIZED, f"{method} on {route.path} does not require auth!"
+
+
+#
+# VALID TESTS
+#
+
+
+def test_auth_success(client, db):
+    create_test_user(db, "johndoe", "abcd1234")
+
+    # Attempt to authenticate
+    auth = client.post("/api/auth/", data={"username": "johndoe", "password": "abcd1234"})
+    token = auth.json()["access_token"]
+    assert auth.status_code == status.HTTP_200_OK
+    assert auth.json()["token_type"] == "bearer"
+    assert token
+
+    # Attempt to use the token to access a protected API endpoint
+    get = client.get("/api/user/", headers={"Authorization": f"Bearer {token}"})
+    assert get.status_code == status.HTTP_200_OK
+    assert len(get.json()) == 1
