bdda972d4b19ba59ef79ea9fa0275c5f77c10e66
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-09-17 16:15:38 -0400

    Adds a test for a missing token

--- backend/app/tests/api/test_auth.py
+++ backend/app/tests/api/test_auth.py
@@ -23,7 +23,7 @@ def test_auth_invalid(client, db, username, password):
     create_test_user(db, "johndoe", "abcd1234")
 
     # Attempt to authenticate
-    auth = client.post("/api/auth/", data={"username": username, "password": password})
+    auth = client.post("/api/auth", data={"username": username, "password": password})
     assert auth.status_code == status.HTTP_401_UNAUTHORIZED
     assert auth.json()["detail"] == "Invalid username or password"
 
@@ -37,14 +37,14 @@ def test_disabled_user(client, db):
     create_test_user(db, "johndoe", "abcd1234")
 
     # Attempt to authenticate
-    auth = client.post("/api/auth/", data={"username": "johndoe", "password": "abcd1234"})
-    token = auth.json()["access_token"]
+    auth = client.post("/api/auth", data={"username": "johndoe", "password": "abcd1234"})
+    access_token = auth.json()["access_token"]
     assert auth.status_code == status.HTTP_200_OK
     assert auth.json()["token_type"] == "bearer"
-    assert token
+    assert access_token
 
     # Attempt to use the token to access a protected API endpoint
-    headers = {"Authorization": f"Bearer {token}"}
+    headers = {"Authorization": f"Bearer {access_token}"}
     get = client.get("/api/user/", headers=headers)
     assert get.status_code == status.HTTP_200_OK
     assert len(get.json()) == 1
@@ -60,7 +60,7 @@ def test_disabled_user(client, db):
     assert len(get.json()) == 1
 
     # However, they will not be able to authenticate again to receive a new token.
-    auth = client.post("/api/auth/", data={"username": "johndoe", "password": "abcd1234"})
+    auth = client.post("/api/auth", data={"username": "johndoe", "password": "abcd1234"})
     assert auth.status_code == status.HTTP_401_UNAUTHORIZED
     assert auth.json()["detail"] == "Invalid username or password"
 
@@ -77,14 +77,14 @@ def test_expired_token(client, db, monkeypatch):
     create_test_user(db, "johndoe", "abcd1234")
 
     # Attempt to authenticate
-    auth = client.post("/api/auth/", data={"username": "johndoe", "password": "abcd1234"})
-    token = auth.json()["access_token"]
+    auth = client.post("/api/auth", data={"username": "johndoe", "password": "abcd1234"})
+    access_token = auth.json()["access_token"]
     assert auth.status_code == status.HTTP_200_OK
     assert auth.json()["token_type"] == "bearer"
-    assert token
+    assert access_token
 
     # Attempt to use the token to access a protected API endpoint
-    get = client.get("/api/user/", headers={"Authorization": f"Bearer {token}"})
+    get = client.get("/api/user/", headers={"Authorization": f"Bearer {access_token}"})
     assert get.status_code == status.HTTP_200_OK
     assert len(get.json()) == 1
 
@@ -92,22 +92,59 @@ def test_expired_token(client, db, monkeypatch):
     time.sleep(2)
 
     # Attempt to use the token to access a protected API endpoint now that the token is expired
-    get = client.get("/api/user/", headers={"Authorization": f"Bearer {token}"})
+    get = client.get("/api/user/", headers={"Authorization": f"Bearer {access_token}"})
     assert get.status_code == status.HTTP_401_UNAUTHORIZED
-    assert get.json()["detail"] == "Token expired"
+    assert get.json()["detail"] == "Access token expired"
+
+
+@pytest.mark.parametrize(
+    "key",
+    [
+        ("username"),
+        ("password"),
+    ],
+)
+def test_missing_required_fields(client, key):
+    create_json = {"username": "johndoe", "password": "abcd1234"}
+    del create_json[key]
+    create = client.post("/api/auth", json=create_json)
+    assert create.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+def test_missing_token(client):
+    # Attempt to access a protected API endpoint without supplying an access token
+    get = client.get("/api/user/")
+    assert get.status_code == status.HTTP_401_UNAUTHORIZED
+    assert get.json()["detail"] == "Not authenticated"
+
+
+def test_wrong_token_type(client, db):
+    create_test_user(db, "johndoe", "abcd1234")
+
+    # Attempt to authenticate
+    auth = client.post("/api/auth", data={"username": "johndoe", "password": "abcd1234"})
+    refresh_token = auth.json()["refresh_token"]
+    assert auth.status_code == status.HTTP_200_OK
+    assert auth.json()["token_type"] == "bearer"
+    assert refresh_token
+
+    # Attempt to use the refresh token to access a protected API endpoint
+    get = client.get("/api/user/", headers={"Authorization": f"Bearer {refresh_token}"})
+    assert get.status_code == status.HTTP_401_UNAUTHORIZED
+    assert get.json()["detail"] == "Invalid token type"
 
 
 @pytest.mark.parametrize(
     "route",
     [route for route in app.routes if route.path.startswith("/api/")],
 )
-def test_invalid_token(client, route):
+def test_missing_route_authentication(client, route):
     """
     This tests every registered API endpoint to ensure that it requires token authentication.
     """
 
     # There are some special endpoints that do not require authentication.
-    if route.path in ["/api/ping", "/api/auth/"]:
+    if route.path in ["/api/ping", "/api/auth"]:
         return
 
     for method in route.methods:
@@ -126,20 +163,6 @@ def test_invalid_token(client, route):
         assert result.status_code == status.HTTP_401_UNAUTHORIZED, f"{method} on {route.path} does not require auth!"
 
 
-@pytest.mark.parametrize(
-    "key",
-    [
-        ("username"),
-        ("password"),
-    ],
-)
-def test_missing_required_fields(client, key):
-    create_json = {"username": "johndoe", "password": "abcd1234"}
-    del create_json[key]
-    create = client.post("/api/auth/", json=create_json)
-    assert create.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
-
-
 #
 # VALID TESTS
 #
@@ -149,13 +172,15 @@ def test_auth_success(client, db):
     create_test_user(db, "johndoe", "abcd1234")
 
     # Attempt to authenticate
-    auth = client.post("/api/auth/", data={"username": "johndoe", "password": "abcd1234"})
-    token = auth.json()["access_token"]
+    auth = client.post("/api/auth", data={"username": "johndoe", "password": "abcd1234"})
+    access_token = auth.json()["access_token"]
+    refresh_token = auth.json()["refresh_token"]
     assert auth.status_code == status.HTTP_200_OK
     assert auth.json()["token_type"] == "bearer"
-    assert token
+    assert access_token
+    assert refresh_token
 
     # Attempt to use the token to access a protected API endpoint
-    get = client.get("/api/user/", headers={"Authorization": f"Bearer {token}"})
+    get = client.get("/api/user/", headers={"Authorization": f"Bearer {access_token}"})
     assert get.status_code == status.HTTP_200_OK
     assert len(get.json()) == 1
