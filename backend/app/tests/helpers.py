54f6d25c0be4d0e97ca1b80b1cde1ea96112ec2a
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-11-01 14:13:23 +0000

    Adds more helper functions

--- backend/app/tests/helpers.py
+++ backend/app/tests/helpers.py
@@ -14,6 +14,7 @@ from db.schemas.alert_tool import AlertTool
 from db.schemas.alert_tool_instance import AlertToolInstance
 from db.schemas.alert_type import AlertType
 from db.schemas.analysis import Analysis
+from db.schemas.analysis_module_type import AnalysisModuleType
 from db.schemas.event import Event
 from db.schemas.event_status import EventStatus
 from db.schemas.node_directive import NodeDirective
@@ -21,6 +22,8 @@ from db.schemas.node_tag import NodeTag
 from db.schemas.node_threat import NodeThreat
 from db.schemas.node_threat_actor import NodeThreatActor
 from db.schemas.node_threat_type import NodeThreatType
+from db.schemas.observable import Observable
+from db.schemas.observable_instance import ObservableInstance
 from db.schemas.observable_type import ObservableType
 from db.schemas.user import User
 from db.schemas.user_role import UserRole
@@ -82,10 +85,6 @@ def create_alert(
     tool: str = "test_tool",
     tool_instance: str = "test_tool_instance",
 ) -> Alert:
-    """
-    Helper function to create an alert. Returns a tuple of (alert_uuid, analysis_uuid)
-    """
-
     if event_time is None:
         event_time = datetime.utcnow()
 
@@ -129,17 +128,84 @@ def create_alert(
     return alert
 
 
-def create_analysis(db: Session) -> Analysis:
-    obj = Analysis(version=uuid.uuid4())
+def create_analysis(
+    db: Session,
+    amt_value: Optional[str] = None,
+    amt_description: Optional[str] = None,
+    amt_extended_version: Optional[dict] = None,
+    amt_manual: bool = False,
+    amt_observable_types: List[str] = None,
+    amt_required_directives: List[str] = None,
+    amt_required_tags: List[str] = None,
+    amt_version: str = "1.0.0",
+) -> Analysis:
+    if amt_value:
+        analysis_module_type = create_analysis_module_type(
+            value=amt_value,
+            description=amt_description,
+            extended_version=amt_extended_version,
+            manual=amt_manual,
+            observable_types=amt_observable_types,
+            required_directives=amt_required_directives,
+            required_tags=amt_required_tags,
+            version=amt_version,
+            db=db,
+        )
+
+        obj = Analysis(analysis_module_type=analysis_module_type, uuid=uuid.uuid4(), version=uuid.uuid4())
+    else:
+        obj = Analysis(uuid=uuid.uuid4(), version=uuid.uuid4())
+
+    db.add(obj)
+    return obj
+
+
+def create_analysis_module_type(
+    value: str,
+    db: Session,
+    description: Optional[str] = None,
+    extended_version: Optional[dict] = None,
+    manual: bool = False,
+    observable_types: List[str] = None,
+    required_directives: List[str] = None,
+    required_tags: List[str] = None,
+    version: str = "1.0.0",
+) -> AnalysisModuleType:
+    existing = crud.read_by_value(value=value, db_table=AnalysisModuleType, db=db, err_on_not_found=False)
+    if existing:
+        return existing
+
+    if observable_types:
+        observable_types = [create_observable_type(value=o, db=db) for o in observable_types]
+    else:
+        observable_types = []
+
+    if required_directives:
+        required_directives = [create_node_directive(value=d, db=db) for d in required_directives]
+    else:
+        required_directives = []
+
+    if required_tags:
+        required_tags = [create_node_tag(value=t, db=db) for t in required_tags]
+    else:
+        required_tags = []
+
+    obj = AnalysisModuleType(
+        value=value,
+        description=description,
+        extended_version=extended_version,
+        manual=manual,
+        observable_types=observable_types,
+        required_directives=required_directives,
+        required_tags=required_tags,
+        uuid=uuid.uuid4(),
+        version=version,
+    )
     db.add(obj)
     return obj
 
 
 def create_event(name: str, db: Session) -> Event:
-    """
-    Helper function to create an event. Returns the event.
-    """
-
     obj = Event(name=name, uuid=uuid.uuid4(), version=uuid.uuid4())
     db.add(obj)
     return obj
@@ -176,6 +242,50 @@ def create_node_threat(value: str, db: Session, types: List[str] = None) -> Node
 
 def create_node_threat_type(value: str, db: Session) -> NodeThreatType:
     return _create_basic_object(db_table=NodeThreatType, value=value, db=db)
+
+
+def create_observable(
+    type: str, value: str, db: Session, expires_on: Optional[datetime] = None, for_detection: bool = False
+) -> Observable:
+    obj = Observable(
+        expires_on=expires_on, for_detection=for_detection, type=create_observable_type(value=type, db=db), value=value
+    )
+    db.add(obj)
+    return obj
+
+
+def create_observable_instance(
+    type: str,
+    value: str,
+    alert: Alert,
+    parent_analysis: Analysis,
+    db: Session,
+    context: Optional[str] = None,
+    performed_analyses: List[Analysis] = None,
+    redirection: Optional[ObservableInstance] = None,
+    time: Optional[datetime] = None,
+) -> ObservableInstance:
+    observable = create_observable(type=type, value=value, db=db)
+
+    if performed_analyses is None:
+        performed_analyses = []
+
+    if time is None:
+        time = datetime.utcnow()
+
+    obj = ObservableInstance(
+        observable=observable,
+        alert_uuid=alert.uuid,
+        parent_analysis=parent_analysis,
+        context=context,
+        performed_analyses=performed_analyses,
+        redirection=redirection,
+        time=time,
+        uuid=uuid.uuid4(),
+        version=uuid.uuid4(),
+    )
+    db.add(obj)
+    return obj
 
 
 def create_observable_type(value: str, db: Session) -> ObservableType:
