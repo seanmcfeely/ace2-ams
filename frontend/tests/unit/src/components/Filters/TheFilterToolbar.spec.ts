fa7854b115fed1d0087450ba345e60246935108b
Holly Fox <fox.holly.13@gmail.com>
2021-11-18 17:11:34 +0000

    Add filter toolbar tests

---
+++ frontend/tests/unit/src/components/Filters/TheFilterToolbar.spec.ts
@@ -0,0 +1,58 @@
+import TheFilterToolbar from "@/components/Filters/TheFilterToolbar.vue";
+import { shallowMount, VueWrapper } from "@vue/test-utils";
+import { createStore } from "vuex";
+
+describe("TheFilterToolbar.vue", () => {
+  let actions: { clearAllFilters: jest.Mock<any, any> },
+    store,
+    wrapper: VueWrapper<any>;
+
+  beforeEach(() => {
+    actions = {
+      clearAllFilters: jest.fn(),
+    };
+    store = createStore({
+      modules: {
+        filters: {
+          namespaced: true,
+          actions: actions,
+        },
+      },
+    });
+
+    wrapper = shallowMount(TheFilterToolbar, {
+      global: {
+        plugins: [store],
+        provide: {
+          filterType() {
+            return "alerts";
+          },
+        },
+      },
+    });
+  });
+
+  it("renders", () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+  it("correctly receives injected data", () => {
+    expect(wrapper.vm.filterType()).toEqual("alerts");
+  });
+  it("correctly maps vuex action", () => {
+    wrapper.vm.clearAllFilters();
+    expect(actions.clearAllFilters).toHaveBeenCalled();
+  });
+  it("calls clearAllFilters on clear and reset", () => {
+    wrapper.vm.clear();
+    wrapper.vm.reset();
+    expect(actions.clearAllFilters).toBeCalledTimes(2);
+
+    // Checks that correct arguments were sent
+    expect(actions.clearAllFilters.mock.calls[0][1]).toEqual({
+      filterType: wrapper.vm.filterType,
+    });
+    expect(actions.clearAllFilters.mock.calls[1][1]).toEqual({
+      filterType: wrapper.vm.filterType,
+    });
+  });
+});
