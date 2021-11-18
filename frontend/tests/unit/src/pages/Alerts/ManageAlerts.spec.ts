23b9019d138b4f63d6dd017e9d3d13a1844bc106
Holly Fox <fox.holly.13@gmail.com>
2021-11-18 17:46:13 +0000

    Add ManageAlerts component tests

--- frontend/tests/unit/src/pages/Alerts/ManageAlerts.spec.ts
+++ frontend/tests/unit/src/pages/Alerts/ManageAlerts.spec.ts
@@ -2,10 +2,17 @@ import ManageAlerts from "@/pages/Alerts/ManageAlerts.vue";
 import TheAlertActionToolbar from "@/components/Alerts/TheAlertActionToolbar.vue";
 import TheFilterToolbar from "@/components/Filters/TheFilterToolbar.vue";
 import TheAlertsTable from "@/components/Alerts/TheAlertsTable.vue";
-import { shallowMount } from "@vue/test-utils";
+import DateRangePicker from "@/components/UserInterface/DateRangePicker.vue";
+import store from "@/store";
+import { mount, shallowMount } from "@vue/test-utils";
 
 describe("ManageAlerts.vue", () => {
-  const wrapper = shallowMount(ManageAlerts);
+  const wrapper = mount(ManageAlerts, {
+    global: {
+      plugins: [store],
+      stubs: ["TheAlertsTable"],
+    },
+  });
   it("renders", () => {
     expect(wrapper.exists()).toBe(true);
   });
@@ -14,5 +21,26 @@ describe("ManageAlerts.vue", () => {
     expect(wrapper.findComponent(TheAlertActionToolbar).exists()).toBe(true);
     expect(wrapper.findComponent(TheFilterToolbar).exists()).toBe(true);
     expect(wrapper.findComponent(TheAlertsTable).exists()).toBe(true);
+  });
+
+  it("provides the correct data to be injected", () => {
+    // All of the data provided by the ManageAlerts component is injected into the DateRangePicker child component
+    // We can therefore find it and check its data for all the injected data
+
+    const datepicker = wrapper.findComponent(DateRangePicker).vm;
+    expect(datepicker.filterType).toEqual("alerts");
+    expect(datepicker.rangeFilterOptions).toEqual([
+      "Event Time",
+      "Insert Time",
+      "Dispositioned Time",
+    ]);
+    expect(datepicker.rangeFilters).toEqual({
+      "Event Time": { start: "eventTimeAfter", end: "eventTimeBefore" },
+      "Insert Time": { start: "insertTimeAfter", end: "insertTimeBefore" },
+      "Dispositioned Time": {
+        start: "dispositionedAfter",
+        end: "dispositionedBefore",
+      },
+    });
   });
 });
