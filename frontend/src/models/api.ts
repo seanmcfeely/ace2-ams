04ed962c9f14eac96c5ea0099b3e5b35e899a9fb
Holly Fox <fox.holly.13@gmail.com>
2021-11-19 19:16:52 +0000

    Add column sorting to alerts table

--- frontend/src/models/api.ts
+++ frontend/src/models/api.ts
@@ -3,5 +3,5 @@ import { alert, alertGetAll, alertFilterParams } from "./alert";
 
 export type anyGetSingle = genericObject & alert;
 export type anyGetAll = genericGetAll & alertGetAll;
-export type pageOptionParams = { limit: number; offset: number };
+export type pageOptionParams = { limit: number; offset: number; sort: string };
 export type getAllParams = pageOptionParams & alertFilterParams;
