b3eb60b94ed4316c0932e3f6b0f9b531641f92a8
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-11-19 17:44:19 +0000

    Fixes accidental issue with Analysis models

--- backend/app/api/models/analysis.py
+++ backend/app/api/models/analysis.py
@@ -17,15 +17,17 @@ class AnalysisBase(NodeBase):
             the case of manually created alerts."""
     )
 
+    details: Optional[Json] = Field(description="A JSON representation of the details produced by the analysis")
+
+    error_message: Optional[type_str] = Field(description="An optional error message that occurred during analysis")
+
+    stack_trace: Optional[type_str] = Field(description="An optional stack trace that occurred during analysis")
+
     summary: Optional[type_str] = Field(description="A short summary/description of what this analysis did or found")
 
 
 class AnalysisCreate(NodeCreate, AnalysisBase):
     alert_uuid: UUID4 = Field(description="The UUID of the alert containing this analysis")
-
-    details: Optional[Json] = Field(description="A JSON representation of the details produced by the analysis")
-
-    error_message: Optional[type_str] = Field(description="An optional error message that occurred during analysis")
 
     # TODO - save for the end, still need to flesh out this functionality
     # event_summary: Optional[AnalysisEventSummary] = Field(
@@ -34,8 +36,6 @@ class AnalysisCreate(NodeCreate, AnalysisBase):
     # )
 
     parent_uuid: Optional[UUID4] = Field(description="The UUID of the observable containing this analysis")
-
-    stack_trace: Optional[type_str] = Field(description="An optional stack trace that occurred during analysis")
 
     uuid: UUID4 = Field(default_factory=uuid4, description="The UUID of the analysis")
 
