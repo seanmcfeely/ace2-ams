596de848695f29a4887b7856810b252eadec2f5d
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-11-24 02:39:58 +0000

    Fixes comment

--- bin/test-backend.sh
+++ bin/test-backend.sh
@@ -6,7 +6,7 @@ set -a
 source "$ACE2_ENV_PATH"
 set +a
 
-# Remove the leading app/ from the command line argument so the path works inside of the container.
+# Remove the leading backend/app/ from the command line argument so the path works inside of the container.
 new_path=${1#backend/app/}
 
 docker-compose up -d
