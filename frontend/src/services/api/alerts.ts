ae940b46c18e7d11d526e2ee665518d1c3f55778
Holly Fox <fox.holly.13@gmail.com>
2021-12-02 09:29:11 -0500

    Add formatting for API

--- frontend/src/services/api/alerts.ts
+++ frontend/src/services/api/alerts.ts
@@ -1,3 +1,21 @@
+import { getAllParams } from "@/models/api";
 import { GenericEndpoint } from "./base";
+import { alertFilters } from "@/etc/constants";
 
-export default new GenericEndpoint("/alert/");
+class alert extends GenericEndpoint {
+    getAll(params: getAllParams) {
+        let param: keyof getAllParams;
+        for (param in params) {
+            const paramValue = params[param];
+            const filterType = alertFilters.find((filter) => {
+                return filter.name === param;
+            });
+            if (filterType && filterType.formatForAPI) {
+                params[param] = filterType.formatForAPI(paramValue) as never;
+            }
+        }
+        return super.getAll(params);
+    }
+}
+
+export default new alert("/alert/");
