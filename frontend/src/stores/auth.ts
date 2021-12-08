3b79df4853479702814782d10c833b1b9d2396d8
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-12-08 01:28:53 +0000

    Adds Auth Pinia store

---
+++ frontend/src/stores/auth.ts
@@ -0,0 +1,43 @@
+import { defineStore } from "pinia";
+import { userRead } from "@/models/user";
+import authApi from "@/services/api/auth";
+
+export const useAuthStore = defineStore({
+  id: "authStore",
+
+  state: () => ({
+    authenticated: false,
+    user: null as unknown as userRead,
+  }),
+
+  getters: {
+    displayName(): string {
+      if (this.user) {
+        return this.user.displayName;
+      }
+
+      return "Unknown User";
+    },
+
+    isAuthenticated(): boolean {
+      return this.authenticated;
+    },
+  },
+
+  actions: {
+    async refreshTokens() {
+      await authApi
+        .refresh()
+        .then(() => {
+          this.authenticated = true;
+        })
+        .catch(() => {
+          this.authenticated = false;
+        });
+    },
+
+    setAuthenticated(value: boolean) {
+      this.authenticated = value;
+    },
+  },
+});
