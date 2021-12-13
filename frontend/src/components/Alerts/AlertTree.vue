054e9d009e660fbd4c382a9e8b715cc9a4b3822f
Holly Fox <fox.holly.13@gmail.com>
2021-12-13 18:46:44 -0500

    Convert AlertTree to composition API

--- frontend/src/components/Alerts/AlertTree.vue
+++ frontend/src/components/Alerts/AlertTree.vue
@@ -9,35 +9,32 @@
   </div>
 </template>
 
-<script>
-  export default {
-    name: "AlertTree",
-    props: {
-      items: { type: Array, required: true },
-    },
+<script setup>
+  import { defineProps } from "vue";
 
-    methods: {
-      treeItemName(item) {
-        if ("analysisModuleType" in item) {
-          return item.analysisModuleType.value;
-        }
+  const props = defineProps({
+    items: { type: Array, required: true },
+  });
 
-        if ("forDetection" in item) {
-          return item.type.value + ": " + item.value;
-        }
-      },
+  function treeItemName(item) {
+    if ("analysisModuleType" in item) {
+      return item.analysisModuleType.value;
+    }
 
-      tagString(item) {
-        let string = "";
+    if ("forDetection" in item) {
+      return item.type.value + ": " + item.value;
+    }
+  }
 
-        if ("tags" in item) {
-          for (let i = 0; i < item.tags.length; i++) {
-            string = string + "(" + item.tags[i].value + ") ";
-          }
-        }
+  function tagString(item) {
+    let string = "";
 
-        return string;
-      },
-    },
-  };
+    if ("tags" in item) {
+      for (let i = 0; i < item.tags.length; i++) {
+        string = string + "(" + item.tags[i].value + ") ";
+      }
+    }
+
+    return string;
+  }
 </script>
