a75d715ae507c87588bf3807a05d378feeef28b9
Holly Fox <fox.holly.13@gmail.com>
2021-12-14 15:35:35 -0500

    Make tree expandable/collapsible

--- frontend/src/components/Alerts/AlertTree.vue
+++ frontend/src/components/Alerts/AlertTree.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
-    <ul>
-      <li v-for="i of items" :id="`ID-${i.treeUuid}`" :key="i.treeUuid">
-        <span>
+    <ul class="p-tree-container">
+      <li :class="containerClass(i)" v-for="(i, index) of items" :id="`ID-${i.treeUuid}`" :key="i.treeUuid">
+        
+        
+        <span class="p-treenode-content">
+          <span v-if="!i.children.length"> <i class="pi pi-fw pi-minus" ></i> 
+          </span>
+          <span v-else>
+          <button :class="p-ripple" type="button" class=" p-link" @click="toggleNodeExpanded(index)" tabindex="-1" >
+                <span :class="toggleIcon(index)"></span>
+            </button>
+            </span>
           <span v-if="isObservable(i)">{{ treeItemName(i) }}</span>
           <router-link v-else :to="getAnalysisLink(i)">{{
             treeItemName(i)
@@ -16,7 +25,7 @@
           </span>
         </span>
 
-        <div>
+        <div class="p-treenode-children" v-if="nodeExpanded(index)">
           <AlertTree :items="i.children" />
         </div>
         <div v-if="showJumpToAnalysis(i)">
@@ -30,13 +39,20 @@
 <script setup>
   import NodeTagVue from "../Node/NodeTag.vue";
 
-  import { defineProps, inject } from "vue";
+  import { defineProps, inject, ref } from "vue";
 
   const observableRefs = inject("observableRefs");
 
   const props = defineProps({
     items: { type: Array, required: true },
   });
+
+  let itemsExpandedStatus = ref({})
+  
+  props.items.forEach((el, index) => {
+    itemsExpandedStatus.value[index] = true;
+  });
+  
 
   function getReferenceObservable(item) {
     if (!observableRefs.value) {
@@ -46,6 +62,18 @@
       return "self";
     }
     return observableRefs.value[item.uuid];
+  }
+
+  function nodeExpanded(index) {
+    return itemsExpandedStatus.value[index];
+  }
+
+  function toggleNodeExpanded(index) {
+    itemsExpandedStatus.value[index] = !itemsExpandedStatus.value[index]
+  }
+
+  function containerClass(item) {
+     return ['p-treenode', {'p-treenode-leaf': !item.children.length}];
   }
 
   function getAnalysisLink(item) {
@@ -62,6 +90,15 @@
   function showJumpToAnalysis(item) {
     return isObservable(item) && getReferenceObservable(item) != "self";
   }
+
+
+  function toggleIcon(index) {
+            return ['p-tree-toggler-icon pi pi-fw', {
+                'pi-chevron-down': nodeExpanded(index),
+                'pi-chevron-right': !nodeExpanded(index)
+            }];
+            
+        }
 
   function jumpToAnalysis(item) {
     const reference = getReferenceObservable(item);
@@ -87,3 +124,73 @@
     return false;
   }
 </script>
+
+<style>
+.p-tree-container {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    overflow: auto;
+}
+.p-treenode-children {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+}
+.p-tree-wrapper {
+    overflow: auto;
+}
+.p-treenode-selectable {
+    cursor: pointer;
+    user-select: none;
+}
+.p-tree-toggler {
+    cursor: pointer;
+    user-select: none;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    position: relative;
+}
+.p-treenode-leaf > .p-treenode-content .p-tree-toggler {
+    visibility: hidden;
+}
+.p-treenode-content {
+    display: flex;
+    align-items: center;
+}
+.p-tree-filter {
+    width: 100%;
+}
+.p-tree-filter-container {
+    position: relative;
+    display: block;
+    width: 100%;
+}
+.p-tree-filter-icon {
+    position: absolute;
+    top: 50%;
+    margin-top: -.5rem;
+}
+.p-tree-loading {
+    position: relative;
+    min-height: 4rem;
+}
+.p-tree .p-tree-loading-overlay {
+    position: absolute;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.p-tree-flex-scrollable {
+    display: flex;
+    flex: 1;
+    height: 100%;
+    flex-direction: column;
+}
+.p-tree-flex-scrollable .p-tree-wrapper {
+    flex: 1;
+}
+</style>
--- frontend/src/pages/Alerts/ViewAlert.vue
+++ frontend/src/pages/Alerts/ViewAlert.vue
@@ -1,7 +1,13 @@
 <!-- ViewAlert.vue -->
 
 <template>
-  <AlertTree v-if="alertStore.openAlert" :items="alertTree" />
+  <Card>
+    <template #content>
+      <div class="p-tree p-component p-tree-wrapper" style="border: none">
+        <AlertTree v-if="alertStore.openAlert" :items="alertTree" />
+      </div>
+    </template>
+  </Card>
 </template>
 
 <script setup>
@@ -13,6 +19,7 @@
     ref,
     readonly,
   } from "vue";
+  import Card from "primevue/card";
   import { useRoute } from "vue-router";
   import { arrayToTree } from "performant-array-to-tree";
 
@@ -64,3 +71,76 @@
 
   provide("observableRefs", readonly(observableRefs));
 </script>
+
+<style>
+  .p-tree-container {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    overflow: auto;
+  }
+  .p-tree {
+    border: none;
+  }
+  .p-treenode-children {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  .p-tree-wrapper {
+    overflow: auto;
+  }
+  .p-treenode-selectable {
+    cursor: pointer;
+    user-select: none;
+  }
+  .p-tree-toggler {
+    cursor: pointer;
+    user-select: none;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    position: relative;
+  }
+  .p-treenode-leaf > .p-treenode-content .p-tree-toggler {
+    visibility: hidden;
+  }
+  .p-treenode-content {
+    display: flex;
+    align-items: center;
+  }
+  .p-tree-filter {
+    width: 100%;
+  }
+  .p-tree-filter-container {
+    position: relative;
+    display: block;
+    width: 100%;
+  }
+  .p-tree-filter-icon {
+    position: absolute;
+    top: 50%;
+    margin-top: -0.5rem;
+  }
+  .p-tree-loading {
+    position: relative;
+    min-height: 4rem;
+  }
+  .p-tree .p-tree-loading-overlay {
+    position: absolute;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .p-tree-flex-scrollable {
+    display: flex;
+    flex: 1;
+    height: 100%;
+    flex-direction: column;
+  }
+  .p-tree-flex-scrollable .p-tree-wrapper {
+    flex: 1;
+  }
+</style>
