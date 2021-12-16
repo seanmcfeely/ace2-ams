8075905e9ed92524b71653f09619873f4a2b5226
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-12-16 15:29:16 -0500

    Adds unit test coverage for TheAlertActionToolbar

--- frontend/tests/unit/src/components/Alerts/TheAlertActionToolbar.spec.ts
+++ frontend/tests/unit/src/components/Alerts/TheAlertActionToolbar.spec.ts
@@ -13,7 +13,7 @@ import { createTestingPinia } from "@pinia/testing";
 describe("TheAlertActionToolbar.vue", () => {
   const wrapper = mount(TheAlertActionToolbar, {
     global: {
-      plugins: [createTestingPinia()],
+      plugins: [createTestingPinia({ stubActions: false })],
       stubs: {
         AssignModal: true,
         CommentModal: true,
@@ -55,5 +55,11 @@ describe("TheAlertActionToolbar.vue", () => {
     expect(buttonsWrapper[5].vm.label).toBe("Remediate");
     expect(buttonsWrapper[6].vm.label).toBe("Delete");
     expect(buttonsWrapper[7].vm.label).toBe("Link");
+  });
+
+  it("opens a modal when the open function is called", () => {
+    expect(wrapper.vm.store.openModals).toStrictEqual([]);
+    wrapper.vm.open("modal1");
+    expect(wrapper.vm.store.openModals).toStrictEqual(["modal1"]);
   });
 });
