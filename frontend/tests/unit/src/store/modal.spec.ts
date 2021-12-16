9a45e831aaf04d7e76e77eaabcb6f128dfc08f03
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2021-12-16 10:46:32 -0500

    Simplifies modal store unit tests

--- frontend/tests/unit/src/store/modal.spec.ts
+++ frontend/tests/unit/src/store/modal.spec.ts
@@ -3,18 +3,18 @@ import { useModalStore } from "@/stores/modal";
 
 createTestingPinia();
 
-describe("modal Getters", () => {
-  it("will return null when there is no open/active modal", () => {
-    const store = useModalStore();
-    store.$reset();
+const store = useModalStore();
 
+describe("modal Getters", () => {
+  beforeEach(() => {
+    store.$reset();
+  });
+
+  it("will return null when there is no open/active modal", () => {
     expect(store.active).toBeNull();
   });
 
   it("will return the name of the currently active modal (first in the list)", () => {
-    const store = useModalStore();
-    store.$reset();
-
     store.openModals = ["modal1", "modal2"];
 
     expect(store.active).toBe("modal1");
@@ -22,10 +22,11 @@ describe("modal Getters", () => {
 });
 
 describe("modal Actions", () => {
-  it("will add a new modal to the front of the open list upon the open action", () => {
-    const store = useModalStore();
+  beforeEach(() => {
     store.$reset();
+  });
 
+  it("will add a new modal to the front of the open list upon the open action", () => {
     expect(store.openModals.length).toBe(0);
     store.open("modal1");
     expect(store.openModals.length).toBe(1);
@@ -35,9 +36,6 @@ describe("modal Actions", () => {
     expect(store.openModals[0]).toBe("modal2");
   });
   it("will remove the given modal from the open list upon the close action", () => {
-    const store = useModalStore();
-    store.$reset();
-
     store.openModals = ["modal1"];
 
     expect(store.openModals.length).toBe(1);
