bf1f50c0519e99d1c3ea37c9a92350b998d491a1
Holly Fox <fox.holly.13@gmail.com>
2021-12-19 17:38:07 -0500

    Remove filter link from NodeTag

--- frontend/src/components/Node/NodeTag.vue
+++ frontend/src/components/Node/NodeTag.vue
@@ -1,27 +1,14 @@
 <template>
-  <router-link class="p-m-1" to="/manage_alerts"
-    ><Chip @click="filterByTag"
-      ><span class="p-chip-text">{{ tag.value }}</span></Chip
-    ></router-link
+  <Chip
+    ><span class="p-chip-text">{{ tag.value }}</span></Chip
   >
 </template>
 
 <script setup>
   import Chip from "primevue/chip";
 
-  import { inject, defineProps } from "vue";
+  import { defineProps } from "vue";
   const props = defineProps({
     tag: { type: Object, required: true },
   });
-
-  import { useFilterStore } from "@/stores/filter";
-  const filterStore = useFilterStore();
-  const filterType = inject("filterType");
-  function filterByTag() {
-    filterStore.setFilter({
-      filterType: filterType,
-      filterName: "tags",
-      filterValue: props.tag,
-    });
-  }
 </script>
--- frontend/tests/unit/src/components/Node/NodeTag.spec.ts
+++ frontend/tests/unit/src/components/Node/NodeTag.spec.ts
@@ -3,8 +3,6 @@ import { mount, VueWrapper } from "@vue/test-utils";
 import { createTestingPinia } from "@pinia/testing";
 import { createRouterMock, injectRouterMock } from "vue-router-mock";
 
-import { useFilterStore } from "@/stores/filter";
-
 const tagStub = { value: "my_tag" };
 
 describe("NodeTag.vue", () => {
@@ -31,14 +29,7 @@ describe("NodeTag.vue", () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  it("receives props and injections as expected", () => {
+  it("receives props  as expected", () => {
     expect(wrapper.vm.props).toEqual({ tag: tagStub });
-    expect(wrapper.vm.filterType).toEqual("alerts");
-  });
-
-  it("filterByTag updates 'tag' filter to prop filter", () => {
-    const store = useFilterStore();
-    wrapper.vm.filterByTag();
-    expect(store.alerts).toEqual({ tags: tagStub });
   });
 });
