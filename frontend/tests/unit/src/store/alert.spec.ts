6eb2629d5150f3b3128522a9ebfb760d35ede133
Holly Fox <fox.holly.13@gmail.com>
2021-12-22 13:21:58 -0500

    alertTable store tests

--- frontend/tests/unit/src/store/alert.spec.ts
+++ frontend/tests/unit/src/store/alert.spec.ts
@@ -4,9 +4,7 @@
 
 import myNock from "@unit/services/api/nock";
 import snakecaseKeys from "snakecase-keys";
-import { userRead } from "@/models/user";
-import { nodeCommentRead } from "@/models/nodeComment";
-import { alertCreate, alertRead } from "@/models/alert";
+import { alertCreate } from "@/models/alert";
 import { useAlertStore } from "@/stores/alert";
 import { createTestingPinia } from "@pinia/testing";
 
@@ -41,125 +39,6 @@ const mockAlert = {
   uuid: "uuid1",
 };
 
-const mockUser: userRead = {
-  defaultAlertQueue: {
-    value: "Default",
-    description: "queue",
-    uuid: "uuid1",
-  },
-  displayName: "Imma Analyst",
-  email: "analyst@company.com",
-  enabled: true,
-  roles: [{ value: "test_role", description: null, uuid: "uuid1" }],
-  timezone: "UTC",
-  username: "analyst",
-  uuid: "uuid1",
-};
-
-const mockComment: nodeCommentRead = {
-  insertTime: new Date(0),
-  nodeUuid: "uuid1",
-  user: mockUser,
-  uuid: "uuid1",
-  value: "A comment",
-};
-
-const mockAPIAlert: alertRead = {
-  comments: [],
-  description: "",
-  directives: [],
-  disposition: null,
-  dispositionTime: null,
-  dispositionUser: null,
-  eventTime: new Date(0),
-  eventUuid: null,
-  insertTime: new Date(0),
-  instructions: null,
-  name: "Test Alert",
-  nodeType: "alert",
-  owner: null,
-  queue: { value: "Default", description: "queue", uuid: "uuid1" },
-  tags: [],
-  threatActor: null,
-  threats: [],
-  tool: null,
-  toolInstance: null,
-  type: { value: "Manual", description: "type", uuid: "uuid1" },
-  uuid: "uuid1",
-  version: "uuid2",
-};
-
-const mockAPIAlertOptionalProperties: alertRead = {
-  comments: [mockComment],
-  description: "A test alert",
-  directives: [],
-  disposition: {
-    value: "False Positive",
-    description: "fp",
-    rank: 1,
-    uuid: "uuid1",
-  },
-  dispositionTime: new Date(0),
-  dispositionUser: mockUser,
-  eventTime: new Date(0),
-  eventUuid: null,
-  insertTime: new Date(0),
-  instructions: null,
-  name: "Test Alert",
-  nodeType: "alert",
-  owner: mockUser,
-  queue: { value: "Default", description: "queue", uuid: "uuid1" },
-  tags: [{ value: "a tag", description: "tag", uuid: "uuid1" }],
-  threatActor: null,
-  threats: [],
-  tool: { value: "GUI", description: null, uuid: "uuid1" },
-  toolInstance: null,
-  type: { value: "Manual", description: "type", uuid: "uuid1" },
-  uuid: "uuid1",
-  version: "uuid2",
-};
-
-// TODO: Move to alertTable store tests
-// describe("alerts utilities", () => {
-//   it("will use default values when parsing an API alert object that has missing properties", () => {
-//     expect(parseAlertSummary(mockAPIAlert)).toStrictEqual({
-//       comments: [],
-//       description: "",
-//       disposition: "OPEN",
-//       dispositionTime: null,
-//       dispositionUser: "None",
-//       eventTime: new Date(0),
-//       insertTime: new Date(0),
-//       name: "Test Alert",
-//       owner: "None",
-//       queue: "Default",
-//       tags: [],
-//       tool: "None",
-//       type: "Manual",
-//       uuid: "uuid1",
-//     });
-//   });
-
-//   it("will use the available values when parsing an API alert object with all optional properties", () => {
-//     expect(parseAlertSummary(mockAPIAlertOptionalProperties)).toStrictEqual({
-//       comments: [mockComment],
-//       description: "A test alert",
-//       disposition: "False Positive",
-//       dispositionTime: new Date(0),
-//       dispositionUser: "Imma Analyst",
-//       eventTime: new Date(0),
-//       insertTime: new Date(0),
-//       name: "Test Alert",
-//       owner: "Imma Analyst",
-//       queue: "Default",
-//       tags: [{ value: "a tag", description: "tag", uuid: "uuid1" }],
-//       tool: "GUI",
-//       type: "Manual",
-//       uuid: "uuid1",
-//     });
-//   });
-// });
-
 describe("alert Actions", () => {
   beforeEach(() => {
     store.$reset();
@@ -184,61 +63,6 @@ describe("alert Actions", () => {
 
     expect(store.openAlert).toEqual(mockAlert);
   });
-
-  // TODO: Move to alertTable store tests
-  // it("will fetch all alerts when getPage is called and no filter options are set", async () => {
-  //   const store = useAlertStore();
-  //   const mockRequest = myNock
-  //     .get("/alert/")
-  //     .reply(200, { items: [mockAPIAlert, mockAPIAlert], total: 2 });
-  //   await store.dispatch("getPage");
-
-  //   expect(mockRequest.isDone()).toEqual(true);
-
-  //   expect(state.openAlert).toBeNull();
-  //   expect(state.totalAlerts).toEqual(2);
-  //   expect(state.visibleQueriedAlerts).toHaveLength(2);
-  // });
-
-  // TODO: Move to alertTable store tests
-  // it("will pass params along when getPage is called and pagination options are set", async () => {
-  //   const state = {
-  //     openAlert: null,
-  //     visibleQueriedAlerts: [],
-  //     totalAlerts: 0,
-  //   };
-  //   const store = new Vuex.Store({ state, mutations, actions });
-  //   const mockRequest = myNock
-  //     .get("/alert/?limit=2&offset=0")
-  //     .reply(200, { items: [mockAPIAlert, mockAPIAlert], total: 2 });
-  //   await store.dispatch("getPage", { limit: 2, offset: 0 });
-
-  //   expect(mockRequest.isDone()).toEqual(true);
-
-  //   expect(state.openAlert).toBeNull();
-  //   expect(state.totalAlerts).toEqual(2);
-  //   expect(state.visibleQueriedAlerts).toHaveLength(2);
-  // });
-
-  // TODO: Move to alertTable store tests
-  // it("will pass params along when getPage is called and sort options are set", async () => {
-  //   const state = {
-  //     openAlert: null,
-  //     visibleQueriedAlerts: [],
-  //     totalAlerts: 0,
-  //   };
-  //   const store = new Vuex.Store({ state, mutations, actions });
-  //   const mockRequest = myNock
-  //     .get("/alert/?sort=event_time%7Casc")
-  //     .reply(200, { items: [mockAPIAlert, mockAPIAlert], total: 2 });
-  //   await store.dispatch("getPage", { sort: "event_time|asc" });
-
-  //   expect(mockRequest.isDone()).toEqual(true);
-
-  //   expect(state.openAlert).toBeNull();
-  //   expect(state.totalAlerts).toEqual(2);
-  //   expect(state.visibleQueriedAlerts).toHaveLength(2);
-  // });
 
   it("will make a request to update an alert given the UUID and update data upon the updateAlert action", async () => {
     const mockRequest = myNock.patch("/alert/uuid1").reply(200);
