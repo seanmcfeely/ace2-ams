d329cd28433def7a885776ebb9141a1c0b034ccf
Holly Fox <fox.holly.13@gmail.com>
2022-01-06 15:02:10 -0500

    Extract filter formatting

--- frontend/src/services/api/alert.ts
+++ frontend/src/services/api/alert.ts
@@ -12,6 +12,30 @@ import { BaseApi } from "./base";
 const api = new BaseApi();
 const endpoint = "/alert/";
 
+export function formatForAPI(
+  params: alertFilterParams,
+): Record<string, string> {
+  const formattedParams = {} as alertFilterParams;
+  for (const param in params) {
+    let paramValue = params[param] as unknown;
+
+    //  check if the given param is specific to alerts and not pageOptionParams, i.e. disposition
+    const filterType = alertFilters.find((filter) => {
+      return filter.name === param;
+    });
+
+    // if so, check if the param's value needs to be formatted, and replace with the newly formatted val
+    if (filterType && filterType.formatForAPI) {
+      if (filterType.formatForAPI) {
+        paramValue = filterType.formatForAPI(paramValue) as never;
+      }
+    }
+
+    formattedParams[param] = paramValue;
+  }
+  return formattedParams;
+}
+
 export const Alert = {
   createAndRead: async (data: alertCreate): Promise<alertTreeRead> =>
     await api.create(endpoint, data, true),
@@ -20,25 +44,12 @@ export const Alert = {
     await api.read(`${endpoint}${uuid}`),
 
   readPage: (params?: alertFilterParams): Promise<alertReadPage> => {
+    let formattedParams = {} as alertFilterParams;
     if (params) {
-      for (const param in params) {
-        const paramValue = params[param] as unknown;
-
-        //  check if the given param is specific to alerts and not pageOptionParams, i.e. disposition
-        const filterType = alertFilters.find((filter) => {
-          return filter.name === param;
-        });
-
-        // if so, check if the param's value needs to be formatted, and replace with the newly formatted val
-        if (filterType && filterType.formatForAPI) {
-          if (filterType.formatForAPI) {
-            params[param] = filterType.formatForAPI(paramValue) as never;
-          }
-        }
-      }
+      formattedParams = formatForAPI(params);
     }
 
-    return api.read(`${endpoint}`, params);
+    return api.read(`${endpoint}`, formattedParams);
   },
 
   update: (uuid: UUID, data: alertUpdate): Promise<void> =>
