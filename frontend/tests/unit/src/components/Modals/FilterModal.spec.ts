91973bbb1bb712b78a34e6b0a4df29d358982f16
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-01-13 14:00:57 -0500

    Tests loadFormFilters being called when FilterModal is mounted

--- frontend/tests/unit/src/components/Modals/FilterModal.spec.ts
+++ frontend/tests/unit/src/components/Modals/FilterModal.spec.ts
@@ -4,12 +4,24 @@ import { createTestingPinia, TestingOptions } from "@pinia/testing";
 
 import { useFilterStore } from "@/stores/filter";
 import { useModalStore } from "@/stores/modal";
+import { alertFilterParams } from "@/models/alert";
 
-function factory(options?: TestingOptions) {
+function factory(args: {
+  filters?: { filterType: "alerts"; filters: alertFilterParams };
+  options?: TestingOptions;
+}) {
+  const testingPinia = createTestingPinia(args.options);
+  const filterStore = useFilterStore();
+  const modalStore = useModalStore();
+
+  if (args.filters) {
+    filterStore.bulkSetFilters(args.filters);
+  }
+
   const wrapper = mount(FilterModal, {
     attachTo: document.body,
     global: {
-      plugins: [createTestingPinia(options)],
+      plugins: [testingPinia],
       provide: {
         filterType: "alerts",
       },
@@ -17,36 +29,54 @@ function factory(options?: TestingOptions) {
     props: { name: "FilterModal" },
   });
 
-  const filterStore = useFilterStore();
-  const modalStore = useModalStore();
-
   return { wrapper, filterStore, modalStore };
 }
 
 describe("FilterModal setup", () => {
   it("renders", () => {
-    const { wrapper } = factory();
+    const { wrapper } = factory({});
 
     expect(wrapper.exists()).toBe(true);
   });
 
   it("sets up data correctly", () => {
-    const { wrapper } = factory();
+    const { wrapper } = factory({});
 
     expect(wrapper.vm.formFilters).toEqual([]);
+  });
+
+  it("executes loadFormFilters when the modal is mounted so preexisting filters are shown in the form", () => {
+    const { wrapper } = factory({
+      filters: {
+        filterType: "alerts",
+        filters: { name: "hello world" },
+      },
+      options: { stubActions: false },
+    });
+
+    expect(wrapper.vm.filterStore.$state[wrapper.vm.filterType]).toEqual({
+      name: "hello world",
+    });
+    expect(wrapper.vm.submitFilters).toEqual({ name: "hello world" });
+    expect(wrapper.vm.formFilters).toEqual([
+      {
+        filterName: "name",
+        filterValue: "hello world",
+      },
+    ]);
   });
 });
 
 describe("FilterModal computed properties", () => {
   it("contains expected computed data when no filters are set", () => {
-    const { wrapper } = factory();
+    const { wrapper } = factory({});
 
     expect(wrapper.vm.submitFilters).toEqual({});
     expect(wrapper.vm.name).toEqual("FilterModal");
   });
 
   it("contains expected computed data when there are filters are set", () => {
-    const { wrapper } = factory({ stubActions: false });
+    const { wrapper } = factory({ options: { stubActions: false } });
 
     wrapper.vm.filterStore.bulkSetFilters({
       filterType: "alerts",
@@ -56,15 +86,15 @@ describe("FilterModal computed properties", () => {
       name: "hello world",
     });
 
-    // if filters had been set, resetFormFilters would have been ran, so simulate that here before checking submitFilters
-    wrapper.vm.resetFormFilters();
+    // if filters had been set, loadFormFilters would have been ran, so simulate that here before checking submitFilters
+    wrapper.vm.loadFormFilters();
     expect(wrapper.vm.submitFilters).toEqual({ name: "hello world" });
   });
 });
 
 describe("FilterModal watchers", () => {
-  it("executes resetFormFilters when filters change", async () => {
-    const { wrapper } = factory({ stubActions: false });
+  it("executes loadFormFilters when filters change", async () => {
+    const { wrapper } = factory({ options: { stubActions: false } });
 
     expect(wrapper.vm.filterStore.$state[wrapper.vm.filterType]).toEqual({});
     expect(wrapper.vm.formFilters).toEqual([]);
@@ -90,7 +120,7 @@ describe("FilterModal watchers", () => {
 
 describe("FilterModal methods", () => {
   it("executes submit as expected when there are new filters", () => {
-    const { wrapper } = factory({ stubActions: false });
+    const { wrapper } = factory({ options: { stubActions: false } });
 
     // Test submitting new filters
     expect(wrapper.vm.filterStore.$state[wrapper.vm.filterType]).toEqual({});
@@ -107,14 +137,14 @@ describe("FilterModal methods", () => {
     });
   });
   it("executes submit as expected when there no filters (cleared)", () => {
-    const { wrapper } = factory({ stubActions: false });
+    const { wrapper } = factory({ options: { stubActions: false } });
 
     wrapper.vm.formFilters = [];
     wrapper.vm.submit();
     expect(wrapper.vm.filterStore.$state[wrapper.vm.filterType]).toEqual({});
   });
   it("executes deleteFormFilter as expected", () => {
-    const { wrapper } = factory();
+    const { wrapper } = factory({});
 
     wrapper.vm.formFilters = [
       {
@@ -145,7 +175,7 @@ describe("FilterModal methods", () => {
     ]);
   });
   it("executes clear as expected", () => {
-    const { wrapper } = factory();
+    const { wrapper } = factory({});
 
     wrapper.vm.formFilters = [
       {
@@ -167,7 +197,7 @@ describe("FilterModal methods", () => {
     expect(wrapper.vm.formFilters).toEqual([]);
   });
   it("executes addNewFilter as expected", () => {
-    const { wrapper } = factory();
+    const { wrapper } = factory({});
 
     expect(wrapper.vm.formFilters).toEqual([]);
     wrapper.vm.addNewFilter();
@@ -178,8 +208,8 @@ describe("FilterModal methods", () => {
       },
     ]);
   });
-  it("executes resetFormFilters as expected", async () => {
-    const { wrapper } = factory({ stubActions: false });
+  it("executes loadFormFilters as expected", async () => {
+    const { wrapper } = factory({ options: { stubActions: false } });
 
     expect(wrapper.vm.filterStore.$state[wrapper.vm.filterType]).toEqual({});
     expect(wrapper.vm.formFilters).toEqual([]);
@@ -189,7 +219,7 @@ describe("FilterModal methods", () => {
       filters: { name: "hello world", owner: "test analyst" },
     });
 
-    wrapper.vm.resetFormFilters();
+    wrapper.vm.loadFormFilters();
     expect(wrapper.vm.formFilters).toEqual([
       {
         filterName: "name",
@@ -202,7 +232,7 @@ describe("FilterModal methods", () => {
     ]);
   });
   it("executes close as expected", () => {
-    const { wrapper } = factory({ stubActions: false });
+    const { wrapper } = factory({ options: { stubActions: false } });
 
     wrapper.vm.filterStore.bulkSetFilters({
       filterType: "alerts",
