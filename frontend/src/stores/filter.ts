3971a0aed924738ae01e9835a6b7cf0feedb80a2
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-01-14 11:11:15 -0500

    Adds events as options to the filter store

--- frontend/src/stores/filter.ts
+++ frontend/src/stores/filter.ts
@@ -4,41 +4,47 @@ import {
   alertFilterValues,
   alertFilterNameTypes,
 } from "@/models/alert";
+import {
+  eventFilterNameTypes,
+  eventFilterParams,
+  eventFilterValues,
+} from "@/models/event";
 
 export const useFilterStore = defineStore({
   id: "filterStore",
 
   state: () => ({
     alerts: {} as alertFilterParams,
+    events: {} as eventFilterParams,
   }),
 
   actions: {
     bulkSetFilters(payload: {
-      filterType: "alerts";
-      filters: alertFilterParams;
+      filterType: "alerts" | "events";
+      filters: alertFilterParams | eventFilterParams;
     }) {
       this.$state[payload.filterType] = payload.filters;
       localStorage.setItem("aceFilters", JSON.stringify(this.$state));
     },
 
     setFilter(payload: {
-      filterType: "alerts";
-      filterName: alertFilterNameTypes;
-      filterValue: alertFilterValues;
+      filterType: "alerts" | "events";
+      filterName: alertFilterNameTypes | eventFilterNameTypes;
+      filterValue: alertFilterValues | eventFilterValues;
     }) {
       this.$state[payload.filterType][payload.filterName] = payload.filterValue;
       localStorage.setItem("aceFilters", JSON.stringify(this.$state));
     },
 
     unsetFilter(payload: {
-      filterType: "alerts";
-      filterName: alertFilterNameTypes;
+      filterType: "alerts" | "events";
+      filterName: alertFilterNameTypes | eventFilterNameTypes;
     }) {
       delete this.$state[payload.filterType][payload.filterName];
       localStorage.setItem("aceFilters", JSON.stringify(this.$state));
     },
 
-    clearAll(payload: { filterType: "alerts" }) {
+    clearAll(payload: { filterType: "alerts" | "events" }) {
       this.$state[payload.filterType] = {};
       localStorage.setItem("aceFilters", JSON.stringify(this.$state));
     },
