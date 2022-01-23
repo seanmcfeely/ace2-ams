20b5f59f2e3a8647baa424a166c8f62cb9d51e8a
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-01-23 14:27:35 -0800

    Removes unnecessary addition to the window object

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,7 +6,6 @@ import Tooltip from "primevue/tooltip";
 
 import router from "@/router";
 import { axiosRefresh } from "@/services/api/axios";
-import { useAuthStore } from "@/stores/auth";
 
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
@@ -16,25 +15,8 @@ import "primevue/resources/themes/saga-blue/theme.css";
 import "camelcase-keys";
 import "snakecase-keys";
 
-declare global {
-  interface Window {
-    Cypress: any;
-    authStore: any;
-  }
-}
-
 (async () => {
   const app = createApp(App).use(createPinia());
-
-  // If the application is running under Cypress, set the authStore in the
-  // window so that it can be accessed by the tests. This is needed when
-  // Cypress signs in programatically instead of via the actual login page.
-  // Parts of the application rely on the authenticated user that the login
-  // page sets in the authStore.
-  if (window.Cypress) {
-    const authStore = useAuthStore();
-    window.authStore = authStore;
-  }
 
   await axiosRefresh().catch(() => {
     console.error("Must reauthenticate");
