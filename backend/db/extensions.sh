88730d2aba3836867a180c85f5661b2eac9fcdaf
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-01-25 17:50:41 -0500

    Adds a PostgreSQL function to truncate all of the test database tables

--- backend/db/extensions.sh
+++ backend/db/extensions.sh
@@ -6,3 +6,21 @@ do
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$db" -c "CREATE EXTENSION IF NOT EXISTS pg_trgm"
     psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "$db" -c "CREATE EXTENSION IF NOT EXISTS pgcrypto"
 done
+
+# Create a script to truncate all of the ace_test tables (used during the E2E tests)
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE OR REPLACE FUNCTION truncate_test_tables() RETURNS void AS \$\$
+    DECLARE
+        dbname VARCHAR := current_database();
+        statements CURSOR FOR
+            SELECT tablename FROM pg_tables
+            WHERE tableowner = 'ace' AND schemaname = 'public';
+    BEGIN
+        IF position('ace_test' in dbname) > 0 THEN
+            FOR stmt IN statements LOOP
+                EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
+            END LOOP;
+        END IF;
+    END;
+    \$\$ LANGUAGE plpgsql;
+EOSQL
