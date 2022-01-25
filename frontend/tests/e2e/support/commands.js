3fa78fa2e65c98dbd0a55cea9fa6c0f2d858a4bc
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-01-25 17:53:15 -0500

    Moves Cypress tests back within the frontend directory

---
+++ frontend/tests/e2e/support/commands.js
@@ -0,0 +1,22 @@
+Cypress.Commands.add("login", () => {
+  cy.request({
+    method: "POST",
+    url: "/api/auth",
+    form: true,
+    body: {
+      username: "analyst",
+      password: "analyst",
+    },
+  });
+});
+
+Cypress.Commands.add("logout", () => {
+  cy.request({
+    method: "GET",
+    url: "/api/auth/logout",
+  });
+});
+
+Cypress.Commands.add("addFormObservable", () => {
+  cy.get("#add-observable").click();
+});
