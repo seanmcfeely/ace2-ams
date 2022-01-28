7684c6b8791c4c55861427d565ee9fdb4ed31eab
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-01-28 14:27:15 -0500

    Removes unnecessary seed API endpoint

--- backend/app/api/routes/test.py
+++ backend/app/api/routes/test.py
@@ -3,12 +3,11 @@ import os
 
 from alembic.config import Config
 from fastapi import APIRouter, Depends, HTTPException, Response, status
-from sqlalchemy import func, select
 from sqlalchemy.orm import Session
 
 from api.routes import helpers
 from db.database import get_db
-from seed import seed_test
+from seed import seed
 
 
 router = APIRouter(
@@ -22,13 +21,16 @@ router = APIRouter(
 #
 
 
-def reset_test_database():
+def reset_test_database(db: Session = Depends(get_db)):
     # Only proceed if the API is running in TESTING mode
     if "TESTING" in os.environ and os.environ["TESTING"].lower() == "yes":
         # Use Alembic to downgrade (delete all the database tables) and then upgrade (rebuild the tables)
         config = Config("alembic.ini")
         alembic.command.downgrade(config, "base")
         alembic.command.upgrade(config, "head")
+
+        # Re-seed the database tables so the tests have a default set of data to work with
+        seed()
 
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     else:
@@ -40,7 +42,7 @@ def reset_test_database():
 helpers.api_route_create(
     router,
     reset_test_database,
-    path="/reset_tables",
+    path="/reset_database",
     dependencies=[],
     responses={
         status.HTTP_204_NO_CONTENT: {
@@ -49,36 +51,6 @@ helpers.api_route_create(
         status.HTTP_403_FORBIDDEN: {
             "description": "Unable to reset the test database when not running in TESTING mode"
         },
-    },
-    status_code=status.HTTP_204_NO_CONTENT,
-)
-
-
-#
-# SEED TEST DATABASE
-#
-
-
-def seed_test_database():
-    # Only proceed if the API is running in TESTING mode
-    if "TESTING" in os.environ and os.environ["TESTING"].lower() == "yes":
-        seed_test()
-
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
-    else:
-        raise HTTPException(status_code=403, detail="Unable to seed the test database when not running in TESTING mode")
-
-
-helpers.api_route_create(
-    router,
-    seed_test_database,
-    path="/seed",
-    dependencies=[],
-    responses={
-        status.HTTP_204_NO_CONTENT: {
-            "description": "The test database was seeded",
-        },
-        status.HTTP_403_FORBIDDEN: {"description": "Unable to seed the test database when not running in TESTING mode"},
     },
     status_code=status.HTTP_204_NO_CONTENT,
 )
