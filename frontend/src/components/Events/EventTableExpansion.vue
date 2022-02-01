a2ac24db25bb6e206204175943e99ebc530ab8f4
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-02-01 15:15:03 -0500

    Adds pagination to the event row expansion table

--- frontend/src/components/Events/EventTableExpansion.vue
+++ frontend/src/components/Events/EventTableExpansion.vue
@@ -2,12 +2,23 @@
 <!-- Contains logic and functionality for displaying data in event row dropdown, currently table of alerts -->
 
 <template>
-  <DataTable :value="alerts">
+  <DataTable
+    :value="alerts"
+    :paginator="true"
+    :rows="10"
+    paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+    :rowsPerPageOptions="[5, 10, 50, 100]"
+    :total-records="alerts.length"
+    responsiveLayout="scroll"
+    currentPageReportTemplate="Showing {first} to {last} of {totalRecords} alerts in the event"
+    :loading="isLoading"
+  >
+    <template #loading>Loading alerts, please wait...</template>
     <Column
-      v-for="(value, name) of columns"
-      :key="name"
-      :field="name"
-      :header="value"
+      v-for="col of columns"
+      :key="col.field"
+      :field="col.field"
+      :header="col.header"
       :sortable="true"
       ><template #body="{ data, field }">
         <AlertTableCell :data="data" :field="field"></AlertTableCell> </template
@@ -26,13 +37,15 @@
   import { Alert } from "@/services/api/alert";
   import { parseAlertSummary } from "@/etc/helpers";
 
-  const alerts = ref(null);
-  const columns = {
-    eventTime: "Event Time",
-    name: "Name",
-    owner: "Owner",
-    disposition: "Disposition",
-  };
+  const alerts = ref([]);
+  const isLoading = ref(false);
+
+  const columns = [
+    { field: "eventTime", header: "Event Time" },
+    { field: "name", header: "Name" },
+    { field: "owner", header: "Owner" },
+    { field: "disposition", header: "Disposition" },
+  ];
 
   onMounted(async () => {
     await getAlerts(props.uuid);
@@ -43,12 +56,14 @@
   });
 
   const getAlerts = async (uuid) => {
+    isLoading.value = true;
     const allAlerts = await Alert.readAllPages({
       eventUuid: uuid,
       sort: "event_time|asc",
     });
 
     alerts.value = allAlerts.map((x) => parseAlertSummary(x));
+    isLoading.value = false;
   };
 </script>
 
