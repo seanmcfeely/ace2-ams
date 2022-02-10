ea51561815ebced552dd436649c09d0929c4038f
Holly Fox <fox.holly.13@gmail.com>
2022-02-09 22:20:20 -0500

    Add nodeThreat create/update to store

--- frontend/src/stores/nodeThreat.ts
+++ frontend/src/stores/nodeThreat.ts
@@ -1,6 +1,8 @@
+import { nodeThreatCreate, nodeThreatUpdate } from "./../models/nodeThreat";
 import { defineStore } from "pinia";
 import { nodeThreatRead } from "@/models/nodeThreat";
 import { NodeThreat } from "@/services/api/nodeThreat";
+import { UUID } from "@/models/base";
 
 export const useNodeThreatStore = defineStore({
   id: "nodeThreatStore",
@@ -16,8 +18,28 @@ export const useNodeThreatStore = defineStore({
   },
 
   actions: {
+    async create(newThreat: nodeThreatCreate) {
+      await NodeThreat.create(newThreat)
+        .then(() => {
+          this.readAll();
+        })
+        .catch((error) => {
+          throw error;
+        });
+    },
+
     async readAll() {
       this.items = await NodeThreat.readAll();
+    },
+
+    async update(uuid: UUID, data: nodeThreatUpdate) {
+      await NodeThreat.update(uuid, data)
+        .then(() => {
+          this.readAll();
+        })
+        .catch((error) => {
+          throw error;
+        });
     },
   },
 });
