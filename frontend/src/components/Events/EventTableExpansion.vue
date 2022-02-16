cb2e64c174dccfa1d3c23dfad5773e09fd6a47f9
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-02-15 21:54:36 -0500

    Adds initial checkbox column to event row expansion table

--- frontend/src/components/Events/EventTableExpansion.vue
+++ frontend/src/components/Events/EventTableExpansion.vue
@@ -8,13 +8,26 @@
     :value="alerts"
     :paginator="true"
     :rows="10"
+    :selection="selectedRows"
     paginator-template="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
     :rows-per-page-options="[5, 10, 50, 100]"
     :total-records="alerts.length"
     responsive-layout="scroll"
     current-page-report-template="Showing {first} to {last} of {totalRecords} alerts in the event"
     data-cy="expandedEvent"
+    @rowSelect="selectedAlertStore.select($event.data.uuid)"
+    @rowUnselect="selectedAlertStore.unselect($event.data.uuid)"
+    @rowSelect-all=""
+    @rowUnselect-all="selectedAlertStore.unselectAll()"
   >
+    <!-- CHECKBOX COLUMN -->
+    <Column
+      id="alert-select"
+      header-style="width: 3em"
+      selection-mode="multiple"
+    />
+
+    <!-- DATA COLUMNS -->
     <Column
       v-for="col of columns"
       :key="col.field"
@@ -28,12 +41,26 @@
 </template>
 
 <script setup>
-  import { computed, defineProps } from "vue";
+  import { computed, defineProps, onBeforeMount, onUnmounted, ref } from "vue";
 
   import Column from "primevue/column";
   import DataTable from "primevue/datatable";
 
   import AlertTableCell from "../Alerts/AlertTableCell.vue";
+  import { useSelectedAlertStore } from "@/stores/selectedAlert";
+
+  const selectedAlertStore = useSelectedAlertStore();
+
+  const selectedRows = ref();
+
+  onBeforeMount(() => {
+    selectedAlertStore.unselectAll();
+  });
+
+  onUnmounted(() => {
+    selectedAlertStore.unselectAll();
+  });
+
   const columns = [
     { field: "eventTime", header: "Event Time" },
     { field: "name", header: "Name" },
