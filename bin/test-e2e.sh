c08640ead6d7ae07e4eb89b027c2597e642b3457
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-02-18 13:52:35 -0500

    Uses vite preview

--- bin/test-e2e.sh
+++ bin/test-e2e.sh
@@ -10,12 +10,5 @@ set +a
 # Bring up the containers (if they aren't already) in testing mode
 docker-compose -f docker-compose.yml -f docker-compose-e2e.yml up -d
 
-# Wait for the GUI to be compiled before running Cypress
-printf "Waiting for GUI to compile"
-until $(curl --output /dev/null --silent --head --fail http://ace2-ams:8080); do
-    printf "."
-    sleep 1
-done
-
 # Run Cypress
 docker exec ace2-ams-gui xvfb-run cypress run --headed --browser chrome
