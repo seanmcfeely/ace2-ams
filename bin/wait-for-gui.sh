f8d18ecad2d6dc3964b0f671fe449d5c22c71612
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-02-18 16:10:12 -0500

    Uses localhost instead of ace2-ams

--- bin/wait-for-gui.sh
+++ bin/wait-for-gui.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 printf "Waiting for the API to be accessible"
-until $(curl --output /dev/null --silent --fail http://ace2-ams:8080/api/ping); do
+until $(curl --output /dev/null --silent --fail http://localhost:8080/api/ping); do
     printf "."
     sleep 1
 done
@@ -9,7 +9,7 @@ done
 printf "\n"
 
 printf "Waiting for the GUI to be accessible"
-until $(curl --output /dev/null --silent --fail http://ace2-ams:8080/login); do
+until $(curl --output /dev/null --silent --fail http://localhost:8080/login); do
     printf "."
     sleep 1
 done
