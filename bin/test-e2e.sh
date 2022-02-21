efb5bcbe31f615afaac71a8dfdb905ed0dc4063c
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-02-21 08:37:43 -0500

    Runs e2e tests in dev mode and improves wait-for-gui logic

--- bin/test-e2e.sh
+++ bin/test-e2e.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Exit if any command fails
+set -e
+
 # Load the environment variables for the dev containers
 ACE2_ENV_PATH="$HOME/.ace2.env"
 set -a
@@ -7,13 +10,8 @@ source "$ACE2_ENV_PATH"
 export TESTING=yes
 set +a
 
-# Ensure that the GUI can be built. It is re-built as part of the docker-compose-e2e.yml entrypoint,
-# but this lets us see the logs in case there are any errors with the build process.
-cd frontend && npm run build || exit $?
-cd ..
-
 # Bring up the containers (if they aren't already) in testing mode
-docker-compose -f docker-compose.yml -f docker-compose-e2e.yml up -d
+docker-compose -f docker-compose.yml up -d
 
 # Wait for things to be ready
 /usr/bin/env bash bin/wait-for-gui.sh
--- bin/wait-for-gui.sh
+++ bin/wait-for-gui.sh
@@ -1,15 +1,33 @@
 #!/usr/bin/env bash
 
 printf "Waiting for the API to be accessible"
+i=0
 until $(curl --output /dev/null --silent --fail http://localhost:8080/api/ping); do
     printf "."
+    ((i=i+1))
+
+    if [ $i -gt 60 ]
+    then
+        printf "Something went wrong! Check the API logs!\n"
+        exit 1
+    fi
+
     sleep 1
 done
 
 printf "\n"
 
 printf "Waiting for the GUI to be accessible"
+i=0
 until $(curl --output /dev/null --silent --fail http://localhost:8080/login); do
     printf "."
+    ((i=i+1))
+
+    if [ $i -gt 60 ]
+    then
+        printf "Something went wrong! Check the GUI logs!\n"
+        exit 1
+    fi
+
     sleep 1
 done
