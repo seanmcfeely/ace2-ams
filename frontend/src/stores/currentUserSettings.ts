a826e545f85ab4c3907f21170341a113dc05ba58
Holly Fox <fox.holly.13@gmail.com>
2022-02-22 10:02:18 -0500

    Fix preferredEventQueue type

--- frontend/src/stores/currentUserSettings.ts
+++ frontend/src/stores/currentUserSettings.ts
@@ -1,9 +1,10 @@
+import { eventQueueRead } from "@/models/eventQueue";
 import { defineStore } from "pinia";
 
 export const useCurrentUserSettingsStore = defineStore({
   id: "currentUserSettingsStore",
 
   state: () => ({
-    preferredEventQueue: null as unknown as boolean,
+    preferredEventQueue: null as unknown as eventQueueRead,
   }),
 });
