f4c0f8b63037291b31a2a8f41671311bcb50feaf
Holly Fox <fox.holly.13@gmail.com>
2022-03-09 12:32:13 -0500

    Remove unneeded/missing prop

--- frontend/src/components/Analysis/AnalysisDetailsBase.vue
+++ frontend/src/components/Analysis/AnalysisDetailsBase.vue
@@ -3,14 +3,4 @@
 
 <template>
   <span> Basic Analysis </span>
-  <br />
-  <span> {{ props.section }} </span>
 </template>
-
-<script setup>
-  import { defineProps } from "vue";
-
-  const props = defineProps({
-    section: { type: String, required: true },
-  });
-</script>
