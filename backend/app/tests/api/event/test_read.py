48bab3c42851a31bd05c670780fda5cad2d91c1e
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-03-17 09:13:12 -0400

    Reorganizes some tests

--- backend/app/tests/api/event/test_read.py
+++ backend/app/tests/api/event/test_read.py
@@ -23,6 +23,88 @@ def test_get_nonexistent_uuid(client_valid_access_token):
     assert get.status_code == status.HTTP_404_NOT_FOUND
 
 
+def test_summary_observable_incorrect_details(client_valid_access_token, db):
+    # Create an event
+    event = helpers.create_event(name="test event", db=db)
+
+    # The observable summary should be empty
+    get = client_valid_access_token.get(f"/api/event/{event.uuid}/summary/observable")
+    assert get.json() == []
+
+    # Add an alert with FA Queue analysis with the wrong details hits keys
+    alert_tree = helpers.create_alert(db=db, event=event)
+    alert_o1 = helpers.create_observable(type="fqdn", value="localhost.localdomain", parent_tree=alert_tree, db=db)
+    helpers.create_analysis(
+        db=db,
+        parent_tree=alert_o1,
+        amt_value="FA Queue Type 1",
+        details={"link": "https://url.to.search/query=asdf", "faqueue_hits": 10},
+    )
+
+    # The observable summary should still be empty since the FA Queue analysis details has the wrong "hits" key
+    get = client_valid_access_token.get(f"/api/event/{event.uuid}/summary/observable")
+    assert get.json() == []
+
+    # Add an alert with FA Queue analysis with the wrong details link keys
+    alert_tree = helpers.create_alert(db=db, event=event)
+    alert_o1 = helpers.create_observable(type="fqdn", value="localhost.localdomain", parent_tree=alert_tree, db=db)
+    helpers.create_analysis(
+        db=db,
+        parent_tree=alert_o1,
+        amt_value="FA Queue Type 1",
+        details={"gui_link": "https://url.to.search/query=asdf", "hits": 10},
+    )
+
+    # The observable summary should have one entry, but its faqueue_link property should be an empty string
+    # since the FA Queue analysis details has the wrong "link" key
+    get = client_valid_access_token.get(f"/api/event/{event.uuid}/summary/observable")
+    assert len(get.json()) == 1
+    assert get.json()[0]["faqueue_hits"] == 10
+    assert get.json()[0]["faqueue_link"] == ""
+
+
+def test_summary_user_incorrect_details(client_valid_access_token, db):
+    # Create an event
+    event = helpers.create_event(name="test event", db=db)
+
+    # The user summary should be empty
+    get = client_valid_access_token.get(f"/api/event/{event.uuid}/summary/user")
+    assert get.json() == []
+
+    # The required keys are "user_id" and "email". The others are optional.
+    # Add an alert with user analysis with the wrong user_id key.
+    alert_tree = helpers.create_alert(db=db, event=event)
+    alert_o1 = helpers.create_observable(
+        type="email_address", value="goodguy@company.com", parent_tree=alert_tree, db=db
+    )
+    helpers.create_analysis(
+        db=db,
+        parent_tree=alert_o1,
+        amt_value="User Analysis",
+        details={"username": "goodguy", "email": "goodguy@company.com"},
+    )
+
+    # The user summary should still be empty since the analysis details has the wrong "user_id" key
+    get = client_valid_access_token.get(f"/api/event/{event.uuid}/summary/user")
+    assert get.json() == []
+
+    # Add an alert with FA Queue analysis with the wrong details email keys
+    alert_tree = helpers.create_alert(db=db, event=event)
+    alert_o1 = helpers.create_observable(
+        type="email_address", value="goodguy@company.com", parent_tree=alert_tree, db=db
+    )
+    helpers.create_analysis(
+        db=db,
+        parent_tree=alert_o1,
+        amt_value="User Analysis",
+        details={"user_id": "goodguy", "email_address": "goodguy@company.com"},
+    )
+
+    # The user summary should still be empty since the analysis details has the wrong "user_id" key
+    get = client_valid_access_token.get(f"/api/event/{event.uuid}/summary/user")
+    assert get.json() == []
+
+
 #
 # VALID TESTS
 #
@@ -257,88 +339,6 @@ def test_summary_user(client_valid_access_token, db):
     assert len(get.json()) == 2
     assert get.json()[0]["email"] == "goodguy@company.com"
     assert get.json()[1]["email"] == "otherguy@company.com"
-
-
-def test_summary_observable_incorrect_details(client_valid_access_token, db):
-    # Create an event
-    event = helpers.create_event(name="test event", db=db)
-
-    # The observable summary should be empty
-    get = client_valid_access_token.get(f"/api/event/{event.uuid}/summary/observable")
-    assert get.json() == []
-
-    # Add an alert with FA Queue analysis with the wrong details hits keys
-    alert_tree = helpers.create_alert(db=db, event=event)
-    alert_o1 = helpers.create_observable(type="fqdn", value="localhost.localdomain", parent_tree=alert_tree, db=db)
-    helpers.create_analysis(
-        db=db,
-        parent_tree=alert_o1,
-        amt_value="FA Queue Type 1",
-        details={"link": "https://url.to.search/query=asdf", "faqueue_hits": 10},
-    )
-
-    # The observable summary should still be empty since the FA Queue analysis details has the wrong "hits" key
-    get = client_valid_access_token.get(f"/api/event/{event.uuid}/summary/observable")
-    assert get.json() == []
-
-    # Add an alert with FA Queue analysis with the wrong details link keys
-    alert_tree = helpers.create_alert(db=db, event=event)
-    alert_o1 = helpers.create_observable(type="fqdn", value="localhost.localdomain", parent_tree=alert_tree, db=db)
-    helpers.create_analysis(
-        db=db,
-        parent_tree=alert_o1,
-        amt_value="FA Queue Type 1",
-        details={"gui_link": "https://url.to.search/query=asdf", "hits": 10},
-    )
-
-    # The observable summary should have one entry, but its faqueue_link property should be an empty string
-    # since the FA Queue analysis details has the wrong "link" key
-    get = client_valid_access_token.get(f"/api/event/{event.uuid}/summary/observable")
-    assert len(get.json()) == 1
-    assert get.json()[0]["faqueue_hits"] == 10
-    assert get.json()[0]["faqueue_link"] == ""
-
-
-def test_summary_user_incorrect_details(client_valid_access_token, db):
-    # Create an event
-    event = helpers.create_event(name="test event", db=db)
-
-    # The user summary should be empty
-    get = client_valid_access_token.get(f"/api/event/{event.uuid}/summary/user")
-    assert get.json() == []
-
-    # The required keys are "user_id" and "email". The others are optional.
-    # Add an alert with user analysis with the wrong user_id key.
-    alert_tree = helpers.create_alert(db=db, event=event)
-    alert_o1 = helpers.create_observable(
-        type="email_address", value="goodguy@company.com", parent_tree=alert_tree, db=db
-    )
-    helpers.create_analysis(
-        db=db,
-        parent_tree=alert_o1,
-        amt_value="User Analysis",
-        details={"username": "goodguy", "email": "goodguy@company.com"},
-    )
-
-    # The user summary should still be empty since the analysis details has the wrong "user_id" key
-    get = client_valid_access_token.get(f"/api/event/{event.uuid}/summary/user")
-    assert get.json() == []
-
-    # Add an alert with FA Queue analysis with the wrong details email keys
-    alert_tree = helpers.create_alert(db=db, event=event)
-    alert_o1 = helpers.create_observable(
-        type="email_address", value="goodguy@company.com", parent_tree=alert_tree, db=db
-    )
-    helpers.create_analysis(
-        db=db,
-        parent_tree=alert_o1,
-        amt_value="User Analysis",
-        details={"user_id": "goodguy", "email_address": "goodguy@company.com"},
-    )
-
-    # The user summary should still be empty since the analysis details has the wrong "user_id" key
-    get = client_valid_access_token.get(f"/api/event/{event.uuid}/summary/user")
-    assert get.json() == []
 
 
 def test_analysis_module_types(client_valid_access_token, db):
