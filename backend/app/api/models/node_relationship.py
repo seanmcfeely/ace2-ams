42eff9a064466a9086916563b2d01809429c0bc6
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-03-24 11:57:55 -0400

    Adds models for node relationships

--- backend/app/api/models/node_relationship.py
+++ backend/app/api/models/node_relationship.py
@@ -1,32 +1,31 @@
 from pydantic import BaseModel, Field, UUID4
-from typing import Optional
 from uuid import uuid4
 
-from api.models import type_str, validators
+from api.models import type_str
+from api.models.node import NodeRead
+from api.models.node_relationship_type import NodeRelationshipTypeRead
 
 
 class NodeRelationshipBase(BaseModel):
     """Represents a node relationship that can be applied to a node (typically an observable)."""
 
-    description: Optional[type_str] = Field(
-        description="An optional human-readable description of the node relationship"
-    )
-
-    value: type_str = Field(description="The value of the node relationship")
+    node_uuid: UUID4 = Field(description="The UUID of the node")
 
 
 class NodeRelationshipCreate(NodeRelationshipBase):
+    related_node_uuid: UUID4 = Field(description="The UUID of the related node")
+
+    type: type_str = Field(description="The type of the node relationship")
+
     uuid: UUID4 = Field(default_factory=uuid4, description="The UUID of the node relationship")
 
 
 class NodeRelationshipRead(NodeRelationshipBase):
     uuid: UUID4 = Field(description="The UUID of the node relationship")
 
+    related_node: NodeRead = Field(description="The related node")
+
+    type: NodeRelationshipTypeRead = Field(description="The type of the node relationship")
+
     class Config:
         orm_mode = True
-
-
-class NodeRelationshipUpdate(NodeRelationshipBase):
-    value: Optional[type_str] = Field(description="The value of the node relationship")
-
-    _prevent_none: classmethod = validators.prevent_none("value")
