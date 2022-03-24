904f1100ee648f0b26248714b418cd32f449efbc
Holly Fox <fox.holly.13@gmail.com>
2022-03-24 11:05:20 -0400

    Add component test script

---
+++ bin/test-components.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Exit if any command fails
+set -e
+
+# Load the environment variables for the dev containers
+ACE2_ENV_PATH="$HOME/.ace2.env"
+set -a
+source "$ACE2_ENV_PATH"
+export TESTING=yes
+set +a
+
+# Bring up the containers (if they aren't already) in testing mode
+docker-compose up -d
+
+# Run Cypress
+docker exec ace2-ams-gui xvfb-run cypress run-ct --headed --browser chrome
+
+bin/disable-test-mode.sh
