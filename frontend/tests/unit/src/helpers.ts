fcc7cc0130d95dd8d73b3cf2612830e0d5f2c84c
Holly Fox <fox.holly.13@gmail.com>
2022-03-28 14:12:27 -0400

    Adjust how custom cypress pinia works

--- frontend/tests/unit/src/helpers.ts
+++ frontend/tests/unit/src/helpers.ts
@@ -9,9 +9,26 @@ export const createCustomPinia = (options?: TestingOptions) => {
   return createTestingPinia(defaultOptions);
 };
 
-export const createCustomCypressPinia = (options?: TestingOptions) => {
-  const defaultOptions: TestingOptions = { createSpy: cy.spy };
-  if (options) {
+export const createCustomCypressPinia = (
+  options?: TestingOptions,
+  count = 0,
+) => {
+  let defaultOptions: TestingOptions = {
+    createSpy: () => cy.stub().as(`stub-${(count += 1)}`),
+  };
+
+  if (options && Object.keys(options).length) {
+    if (!options.stubActions) {
+      defaultOptions = {
+        createSpy: (fn) => {
+          if (fn) {
+            return cy.spy(fn).as(`spy-${(count += 1)}`);
+          } else {
+            return cy.spy().as(`spy-${(count += 1)}`);
+          }
+        },
+      };
+    }
     return createTestingPinia({ ...options, ...defaultOptions });
   }
 
