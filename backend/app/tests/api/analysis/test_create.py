97190d6e4c122673fecebafd51b63a2c1820a4ed
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-03-29 13:56:58 -0400

    Adds test for invalid sandbox analysis

--- backend/app/tests/api/analysis/test_create.py
+++ backend/app/tests/api/analysis/test_create.py
@@ -132,6 +132,23 @@ def test_create_invalid_faqueue_analysis(client_valid_access_token, db):
     assert create.status_code == status.HTTP_400_BAD_REQUEST
 
 
+def test_create_invalid_sandbox_analysis(client_valid_access_token, db):
+    node_tree = helpers.create_alert(db=db)
+    analysis_module_type = helpers.create_analysis_module_type(value="Sandbox Analysis - Sandbox1", db=db)
+
+    # Create the analysis - it is missing the required "filename" key
+    create = client_valid_access_token.post(
+        "/api/analysis/",
+        json={
+            "analysis_module_type": str(analysis_module_type.uuid),
+            "node_tree": {"parent_tree_uuid": str(node_tree.uuid), "root_node_uuid": str(node_tree.root_node_uuid)},
+            "details": json.dumps({"sandbox_url": "http://url.to.sandbox.report"}),
+        },
+    )
+
+    assert create.status_code == status.HTTP_400_BAD_REQUEST
+
+
 def test_create_invalid_user_analysis(client_valid_access_token, db):
     node_tree = helpers.create_alert(db=db)
     analysis_module_type = helpers.create_analysis_module_type(value="User Analysis", db=db)
