61f0042b5e13530864ff88079a00c9e526097515
Holly Fox <fox.holly.13@gmail.com>
2022-04-05 16:20:54 -0400

    Add TheAlertDetails component tests

--- frontend/tests/component/src/components/Alerts/TheAlertDetails.spec.ts
+++ frontend/tests/component/src/components/Alerts/TheAlertDetails.spec.ts
@@ -8,33 +8,60 @@ import PrimeVue from "primevue/config";
 
 import TheAlertDetails from "@/components/Alerts/TheAlertDetails.vue";
 import router from "@/router/index";
+import { alertReadFactory } from "@mocks/alert";
+import { alertRead } from "@/models/alert";
+import { genericObjectReadFactory } from "@mocks/genericObject";
+
+function factory(initialAlertStoreState: {
+  open: null | alertRead;
+  requestReload: boolean;
+}) {
+  return mount(TheAlertDetails, {
+    global: {
+      plugins: [
+        PrimeVue,
+        createCustomCypressPinia({
+          initialState: { alertStore: initialAlertStoreState },
+        }),
+        router,
+      ],
+    },
+  });
+}
 
 describe("TheAlertDetails", () => {
-  it("renders", () => {
-    mount(TheAlertDetails, {
-      global: {
-        plugins: [
-          PrimeVue,
-          createCustomCypressPinia({
-            initialState: {
-              alertStore: {
-                open: {
-                  name: "test",
-                  tags: [],
-                  childTags: [],
-                  comments: [],
-                  insert_time: "03/23/2022",
-                  uuid: "test",
-                  queue: { value: "test" },
-                  type: { value: "test" },
-                },
-                requestReload: false,
-              },
-            },
-          }),
-          router,
-        ],
-      },
+  it("renders correctly when there is not an open alert", () => {
+    factory({
+      open: null,
+      requestReload: false,
     });
+    cy.contains("Test Alert").should("not.exist");
+  });
+  it("renders correctly when there is an open alert", () => {
+    factory({
+      open: alertReadFactory({
+        tags: [genericObjectReadFactory({ value: "TestTag" })],
+      }),
+      requestReload: false,
+    });
+    cy.contains("Test Alert").should("be.visible");
+    cy.get("button .pi-link").should("be.visible");
+    cy.get("tr").should("have.length", 10);
+    cy.contains("Insert Time")
+      .siblings()
+      .should("have.text", "3/24/2022, 12:00:00 AM");
+    cy.contains("Event Time")
+      .siblings()
+      .should("have.text", "3/24/2022, 12:00:00 AM");
+    cy.contains("Tool").siblings().should("have.text", "testAlertTool");
+    cy.contains("Tool Instance")
+      .siblings()
+      .should("have.text", "testAlertToolInstance");
+    cy.contains("Type").siblings().should("have.text", "testAlertType");
+    cy.contains("Disposition").siblings().should("have.text", "OPEN");
+    cy.get("td:contains(Event)").eq(1).siblings().should("have.text", "None");
+    cy.contains("Queue").siblings().should("have.text", "testAlertQueue");
+    cy.contains("Owner").siblings().should("have.text", "None");
+    cy.contains("Comments").siblings().should("have.text", "None");
   });
 });
