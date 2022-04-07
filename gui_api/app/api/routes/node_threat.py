aa8e7f829fbd65159948bd5a2cb437096544fab0
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-04-06 21:20:56 -0400

    Adds GUI API endpoint for updating node threats

--- gui_api/app/api/routes/node_threat.py
+++ gui_api/app/api/routes/node_threat.py
@@ -6,7 +6,7 @@ from uuid import UUID
 
 from api import db_api
 from api.routes import helpers
-from api_models.node_threat import NodeThreatCreate, NodeThreatRead
+from api_models.node_threat import NodeThreatCreate, NodeThreatRead, NodeThreatUpdate
 
 
 router = APIRouter(
@@ -48,3 +48,25 @@ def get_all_node_threats(limit: Optional[int] = Query(50, le=100), offset: Optio
 
 helpers.api_route_read(router, get_node_threat, NodeThreatRead)
 helpers.api_route_read_all(router, get_all_node_threats, NodeThreatRead)
+
+
+#
+# UPDATE
+#
+
+
+def update_node_threat(
+    uuid: UUID,
+    node_threat: NodeThreatUpdate,
+    request: Request,
+    response: Response,
+):
+    db_api.patch(
+        path=f"/node/threat/{uuid}",
+        payload=json.loads(node_threat.json(exclude_unset=True)),
+    )
+
+    response.headers["Content-Location"] = request.url_for("get_node_threat", uuid=uuid)
+
+
+helpers.api_route_update(router, update_node_threat)
