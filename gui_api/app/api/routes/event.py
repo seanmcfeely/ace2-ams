dcdbb05bb2512f8d1c67a87393ab9b7135521925
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-04-08 15:48:23 -0400

    get_event_history needs pagination parameters

--- gui_api/app/api/routes/event.py
+++ gui_api/app/api/routes/event.py
@@ -191,8 +191,9 @@ def get_event(uuid: UUID):
     return db_api.get(path=f"/event/{uuid}")
 
 
-def get_event_history(uuid: UUID):
-    return db_api.get(f"/event/{uuid}/history")
+def get_event_history(uuid: UUID, limit: Optional[int] = Query(50, le=100), offset: Optional[int] = Query(0)):
+    query_params = f"?limit={limit}&offset={offset}"
+    return db_api.get(f"/event/{uuid}/history{query_params}")
 
 
 helpers.api_route_read_all(router, get_all_events, EventRead)
