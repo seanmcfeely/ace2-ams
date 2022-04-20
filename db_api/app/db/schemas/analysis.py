0da895ee7f0ad66c7717cd3f81fab2abddeaa6b5
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-04-20 14:33:43 -0400

    Adds ExcludeConstraint to analysis

--- db_api/app/db/schemas/analysis.py
+++ db_api/app/db/schemas/analysis.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, DateTime, ForeignKey, String
-from sqlalchemy.dialects.postgresql import JSONB, UUID
+from sqlalchemy import Column, DateTime, ForeignKey, Index, String
+from sqlalchemy.dialects.postgresql import ExcludeConstraint, JSONB, TSTZRANGE, UUID
 from sqlalchemy.orm import deferred, relationship
 
 from api_models.analysis import AnalysisNodeTreeRead
@@ -15,7 +15,7 @@ class Analysis(Node):
 
     analysis_module_type_uuid = Column(UUID(as_uuid=True), ForeignKey("analysis_module_type.uuid"), nullable=False)
 
-    cached_until = Column(DateTime(timezone=True), index=True, nullable=False)
+    cached_during = Column(TSTZRANGE(), nullable=False)
 
     # Using deferred means that when you query the Analysis table, you will not select the details field unless
     # you explicitly ask for it. This is so that we can more efficiently load alert trees without selecting
@@ -33,6 +33,26 @@ class Analysis(Node):
     summary = Column(String)
 
     __mapper_args__ = {"polymorphic_identity": "analysis", "polymorphic_load": "inline"}
+
+    __table_args__ = (
+        Index(
+            "analysis_module_type_observable_cached_during_idx",
+            analysis_module_type_uuid,
+            parent_observable_uuid,
+            cached_during,
+        ),
+        ExcludeConstraint(
+            ("analysis_module_type_uuid", "="),
+            ("parent_observable_uuid", "="),
+            ("cached_during", "&&"),
+            name="cached_analysis_uc",
+            using="gist",
+        ),
+    )
+
+    @property
+    def cached_until(self):
+        return self.cached_during.upper
 
     def serialize_for_node_tree(self) -> AnalysisNodeTreeRead:
         return AnalysisNodeTreeRead(**self.__dict__)
