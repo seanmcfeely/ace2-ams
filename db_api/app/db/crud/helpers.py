d1c6f5212b5d4551a475587f93d7acd1b47531c3
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-04-28 15:04:04 -0400

    Adds some basic docstrings

--- db_api/app/db/crud/helpers.py
+++ db_api/app/db/crud/helpers.py
@@ -2,18 +2,24 @@ from datetime import datetime, timezone
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.decl_api import DeclarativeMeta
+from typing import Any, Optional
 from uuid import UUID
 
 
 def read_by_uuid(db_table: DeclarativeMeta, uuid: UUID, db: Session):
+    """Returns the object with the specific UUID from the given database table."""
     return db.execute(select(db_table).where(db_table.uuid == uuid)).scalars().one()
 
 
-def read_by_value(db_table: DeclarativeMeta, value: str, db: Session):
+def read_by_value(db_table: DeclarativeMeta, value: str, db: Session) -> Optional[Any]:
+    """Returns the object with the specific value (if it exists) from the given database table."""
     return db.execute(select(db_table).where(db_table.value == value)).scalars().one_or_none()
 
 
-def read_by_values(db_table: DeclarativeMeta, values: list[str], db: Session) -> list:
+def read_by_values(db_table: DeclarativeMeta, values: list[str], db: Session) -> list[Any]:
+    """Returns all of the objects with the specific values (if they exist) from the given database table.
+    Raises an exception if the number of objects returned from the database does not match the number of
+    unique given values."""
     if not values:
         return []
 
@@ -24,4 +30,5 @@ def read_by_values(db_table: DeclarativeMeta, values: list[str], db: Session) ->
 
 
 def utcnow() -> datetime:
+    """Returns a timezone-aware version of datetime.utcnow()."""
     return datetime.now(timezone.utc)
