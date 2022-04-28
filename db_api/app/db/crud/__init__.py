83d2c7f7f788cfee67dcb975951f40f46f9b5016
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-04-28 14:29:16 -0400

    Removes old nested alert tree code

--- db_api/app/db/crud/__init__.py
+++ db_api/app/db/crud/__init__.py
@@ -3,18 +3,25 @@ from datetime import datetime, timezone
 from fastapi import HTTPException, status
 from fastapi_pagination.ext.sqlalchemy_future import paginate
 from pydantic import BaseModel
-from pydantic.types import UUID4
 from sqlalchemy import delete as sql_delete, select, update as sql_update
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import undefer, Session
 from sqlalchemy.orm.decl_api import DeclarativeMeta
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.sql.expression import join
-from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Union
 from uuid import UUID, uuid4
 
-from api_models.node import NodeRead, NodeTreeMetadata
+from api_models.node import NodeTreeMetadata
 from core.auth import verify_password
+from db.crud import alert
+from db.crud import analysis
+from db.crud import analysis_module_type
+from db.crud import helpers
+from db.crud import node_directive
+from db.crud import node_tag
+from db.crud import observable_type
+from db.crud import observable
 from db.schemas.alert import AlertHistory
 from db.schemas.analysis import Analysis
 from db.schemas.event import EventHistory
@@ -399,49 +406,6 @@ def read_by_values(values: List[str], db_table: DeclarativeMeta, db: Session):
     return resources
 
 
-def read_node_tree(root_node_uuid: UUID, db: Session) -> dict:
-    """Returns a list of Node objects that comprise a Node Tree. The returned objects
-    are manually serialized into their Pydantic models since Pydantic cannot handle
-    returning a list of multiple types of objects in this case."""
-
-    # While we can query the Node table and get back a list of various types of objects (Analysis, Observable, etc)
-    # due to the polymorphic loading, Pydantic cannot handle returning multiple types of similar objects in a list.
-    # If you try to use a Union of types in the response model, it will try to match the object with the first type
-    # in the Union that fits. And since all of the Node child objects share certain properties, there is no way for
-    # Pydantic to properly match the types.
-    #
-    # It's not an ideal solution, but the only way I've found around this is to query the database for all the Node
-    # objects and then manually check their types to serialize them into their correct Pydantic models. Then the
-    # response model for this API endpoint is simply a "dict" instead of a more specific type.
-    #
-    # Source: https://github.com/samuelcolvin/pydantic/issues/514#issuecomment-491298181
-
-    node_tree_and_nodes: List[Tuple[NodeTree, Node]] = (
-        db.execute(
-            select([NodeTree, Node])
-            .select_from(join(NodeTree, Node, NodeTree.node_uuid == Node.uuid))
-            .where(NodeTree.root_node_uuid == root_node_uuid)
-        )
-        .unique()
-        .fetchall()
-    )
-
-    if not node_tree_and_nodes:
-        raise HTTPException(status_code=404, detail=f"Tree for {root_node_uuid} does not exist.")
-
-    node_tree_nodes = []
-    for leaf, node in node_tree_and_nodes:
-        # Inject the tree information into the Node
-        node.node_metadata = leaf.node_metadata
-        node.tree_uuid = leaf.uuid
-        node.parent_tree_uuid = leaf.parent_tree_uuid
-
-        # Serialize the database objects into their correct Pydantic models
-        node_tree_nodes.append(node.serialize_for_node_tree())
-
-    return unflatten_node_tree(node_tree_nodes)
-
-
 def read_node_tree_nodes(node_type: str, root_node_uuids: list[UUID], db: Session) -> list[dict]:
     """Returns a list of Node objects of the given type that are in the given NodeTrees.
     The returned objects are manually serialized into their Pydantic models since Pydantic
@@ -462,66 +426,6 @@ def read_node_tree_nodes(node_type: str, root_node_uuids: list[UUID], db: Sessio
         raise HTTPException(status_code=404, detail=f"No Nodes of type {node_type} in trees {root_node_uuids}")
 
     return [node.__dict__ for node in nodes]
-
-
-def unflatten_node_tree(node_tree_nodes: List[NodeRead]) -> dict:
-    """Takes a flat list of nodes from a NodeTree after they've been serialized into
-    their Pydantic models and converts it into a nested structure.
-
-    Adapted from: https://www.npmjs.com/package/performant-array-to-tree"""
-
-    root_node = {}
-
-    # Used to store the already processed nodes where the node's UUID is the key
-    lookup: Dict[UUID4, dict] = {}
-
-    for node in node_tree_nodes:
-        # If this node is not already in the lookup table, add a preliminary item for it
-        if node.tree_uuid not in lookup:
-            lookup[node.tree_uuid] = {"children": []}
-
-        # Add the node's data to the item in the lookup table. Remember the nodes in the
-        # list are Pydantic model objects, so to update the dictionary in the lookup table,
-        # we have to work with the Pydantic object's dictionary as well.
-        #
-        # Also, exclude_unset is used because the Node objects out of the database do not
-        # actually have a "children" field, and since its Pydantic model uses an empty list
-        # as the default_factory, updating the item in the lookup table would overwrite
-        # the children key with an empty list.
-        lookup[node.tree_uuid].update(node.dict(exclude_unset=True))
-
-        # If the node does not have a parent, add it as the root node
-        if not node.parent_tree_uuid:
-            root_node = lookup[node.tree_uuid]
-        else:
-            # If the parent is not already in the lookup table, add a preliminary item for it
-            if node.parent_tree_uuid not in lookup:
-                lookup[node.parent_tree_uuid] = {"children": []}
-
-            # Add the node to its parent
-            lookup[node.parent_tree_uuid]["children"].append(lookup[node.tree_uuid])
-
-    # Now that the tree is unflattened, we need to walk it to mark which of the Nodes have
-    # already appeared in the tree. This is useful for when you might not want to display or
-    # process a Node in the tree if it is a duplicate.
-    #
-    # Adapted from: https://www.geeksforgeeks.org/preorder-traversal-of-n-ary-tree-without-recursion/
-    unique_uuids = []
-    unvisited = [{"uuid": "root", "children": [root_node]}]
-
-    while unvisited:
-        current = unvisited.pop(0)
-
-        if current["uuid"] in unique_uuids:
-            current["first_appearance"] = False
-        else:
-            current["first_appearance"] = True
-            unique_uuids.append(current["uuid"])
-
-        for idx in range(len(current["children"]) - 1, -1, -1):
-            unvisited.insert(0, current["children"][idx])
-
-    return root_node
 
 
 #
