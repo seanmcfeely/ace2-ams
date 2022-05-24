40bdd333b98370cff52b167a320abfff61610c29
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-05-24 10:44:07 -0400

    Fixes node endpoint tests

--- db_api/app/api/routes/node.py
+++ db_api/app/api/routes/node.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from uuid import UUID
 
@@ -6,6 +6,7 @@ from api.routes import helpers
 from api_models.node import NodeVersion
 from db import crud
 from db.database import get_db
+from exceptions.db import UuidNotFoundInDatabase
 
 
 router = APIRouter(
@@ -20,7 +21,10 @@ router = APIRouter(
 
 
 def get_node_version(uuid: UUID, db: Session = Depends(get_db)):
-    return crud.node.read_by_uuid(uuid=uuid, db=db)
+    try:
+        return crud.node.read_by_uuid(uuid=uuid, db=db)
+    except UuidNotFoundInDatabase as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e)) from e
 
 
 helpers.api_route_read(router, get_node_version, NodeVersion, path="/{uuid}/version")
