8bf79681bf34510c69c66467ef3b6cf121f1e0bf
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-05-31 17:01:03 -0400

    Removes history_username as a query parameter

--- gui_api/app/tests/api/alert/test_create.py
+++ gui_api/app/tests/api/alert/test_create.py
@@ -1,5 +1,5 @@
 def test_create_alert(client_valid_access_token, requests_mock):
-    requests_mock.post("http://db-api/api/alert/?history_username=analyst", json={"uuid": "uuid1"})
+    requests_mock.post("http://db-api/api/alert/", json={"uuid": "uuid1"})
 
     client_valid_access_token.post(
         "/api/alert/",
@@ -13,4 +13,4 @@ def test_create_alert(client_valid_access_token, requests_mock):
 
     assert (len(requests_mock.request_history)) == 2
     assert requests_mock.request_history[1].method == "POST"
-    assert requests_mock.request_history[1].url == "http://db-api/api/alert/?history_username=analyst"
+    assert requests_mock.request_history[1].url == "http://db-api/api/alert/"
--- gui_api/app/tests/api/node_comment/test_update.py
+++ gui_api/app/tests/api/node_comment/test_update.py
@@ -6,7 +6,7 @@ def test_update_node_comment(client_valid_access_token, requests_mock):
     node_comment_uuid = str(uuid4())
 
     requests_mock.patch(
-        f"http://db-api/api/node/comment/{node_comment_uuid}?history_username=analyst",
+        f"http://db-api/api/node/comment/{node_comment_uuid}",
         status_code=status.HTTP_204_NO_CONTENT,
     )
 
@@ -16,7 +16,4 @@ def test_update_node_comment(client_valid_access_token, requests_mock):
 
     assert (len(requests_mock.request_history)) == 2
     assert requests_mock.request_history[1].method == "PATCH"
-    assert (
-        requests_mock.request_history[1].url
-        == f"http://db-api/api/node/comment/{node_comment_uuid}?history_username=analyst"
-    )
+    assert requests_mock.request_history[1].url == f"http://db-api/api/node/comment/{node_comment_uuid}"
