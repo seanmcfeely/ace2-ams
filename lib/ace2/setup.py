a3d73c46bc71fc4fbf90f3fb0528834323f5019b
cole robinette <cole.robinette@53.com>
2022-05-31 09:15:16 -0400

    using src dir

--- lib/ace2/setup.py
+++ lib/ace2/setup.py
@@ -2,8 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='ace2',
-    packages=['ace2'],
+    packages=find_packages('src'),
+    package_dir={'', 'src'},
     python_requires='>=3.9',
+    include_package_data=True,
+    zip_safe=False,
     install_requires = [
         'boto3',
         'pydantic',
