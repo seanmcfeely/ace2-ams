9988fc37cf7262ff8e5ddc1ba0807d7112588e47
Nicholas Reppen <nicholas.reppen@53.com>
2022-06-01 09:26:13 -0400

    fix tf issue

--- aws/terraform/s3/observables-bucket.tf
+++ aws/terraform/s3/observables-bucket.tf
@@ -4,15 +4,16 @@ resource "aws_s3_bucket" "file_storage_bucket" {
   versioning {
     enabled = true
   }
+}
 
-  aws_s3_bucket_server_side_encryption_configuration {
+resource "aws_s3_bucket_server_side_encryption_configuration" "file_storage_encryption" {
+  bucket = aws_s3_bucket.file_storage_bucket.bucket
     rule {
       apply_server_side_encryption_by_default {
         kms_master_key_id = aws_kms_key.ice2_s3.arn
         sse_algorithm     = "aws:kms"
       }
     }
-  }
 }
 
 resource "aws_s3_bucket_policy" "allow_access_from_own_account" {
