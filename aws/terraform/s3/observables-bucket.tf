46faa2fa335749ec2b93e5f8738b1957c5eeb427
Nicholas Reppen <nicholas.reppen@53.com>
2022-06-01 12:50:00 -0400

    fix versioning complaint

--- aws/terraform/s3/observables-bucket.tf
+++ aws/terraform/s3/observables-bucket.tf
@@ -1,9 +1,10 @@
 resource "aws_s3_bucket" "file_storage_bucket" {
   bucket = "ice2-file-storage"
+}
 
-  versioning {
-    enabled = true
-  }
+resource "aws_s3_bucket_versioning" "file_storage_versioning" {
+  bucket = aws_s3_bucket.file_storage_bucket.bucket
+  enabled = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "file_storage_encryption" {
