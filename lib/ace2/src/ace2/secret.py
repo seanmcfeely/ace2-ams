d27078c7f7c8097f596c161adad10f162c8ba2a9
cole robinette <cole.robinette@53.com>
2022-06-02 17:17:38 -0400

    added docstrings and type hinting to secret

--- lib/ace2/src/ace2/secret.py
+++ lib/ace2/src/ace2/secret.py
@@ -1,13 +1,27 @@
 from boto3.session import Session
 
 class Secret(str):
+    ''' special model for translating a aws secrets manager id into the secret value '''
+
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
 
     @classmethod
-    def validate(cls, value):
+    def validate(cls, value:str) -> str:
+        ''' translates the secret id into a secret value
+
+        Args:
+            value: the secret id to translate
+
+        Returns:
+            the secret value string
+        '''
+
+        # get the secret value from the secret id
         session = Session()
         client = session.client(service_name='secretsmanager', region_name=session.region_name)
         secret = client.get_secret_value(SecretId=value)
+
+        # use the secret value as the value for this field
         return secret['SecretString']
