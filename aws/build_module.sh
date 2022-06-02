c0929bef70341e88ca5aa809d132c9b92ee10b1c
cole robinette <cole.robinette@53.com>
2022-06-02 07:24:16 -0400

    fixed image name being uppercase

--- aws/build_module.sh
+++ aws/build_module.sh
@@ -3,8 +3,9 @@
 # get path to module without trailing slashes
 path=$(echo $1 | sed 's:/*$::')
 
-# create image name from module path basename
+# create image name from lowercased module path basename
 name=${path##*/}
+name=$(echo $name | awk '{print tolower($0)}')
 
 # build module image
 docker build --build-arg module=$path -t analysis-module-$name -f aws/Dockerfile.analysis_module .
