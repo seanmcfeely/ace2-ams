d84bccadf96557e34194e5e6ffe03e70f5f3193b
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-06-03 10:03:28 -0400

    Adds optional description

--- db_api/app/tests/factory/alert_disposition.py
+++ db_api/app/tests/factory/alert_disposition.py
@@ -1,8 +1,11 @@
 from sqlalchemy.orm import Session
+from typing import Optional
 
 from api_models.alert_disposition import AlertDispositionCreate
 from db import crud
 
 
-def create_or_read(value: str, rank: int, db: Session):
-    return crud.alert_disposition.create_or_read(model=AlertDispositionCreate(value=value, rank=rank), db=db)
+def create_or_read(value: str, rank: int, db: Session, description: Optional[str] = None):
+    return crud.alert_disposition.create_or_read(
+        model=AlertDispositionCreate(description=description, value=value, rank=rank), db=db
+    )
