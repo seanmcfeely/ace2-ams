47bda2a461d3d731d119f78bfe0fca540dd09397
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-06-03 12:06:32 -0400

    Simplifies expression

--- db_api/app/db/database.py
+++ db_api/app/db/database.py
@@ -11,9 +11,7 @@ database_url = get_settings().database_url
 if is_in_testing_mode():
     database_url = get_settings().database_test_url
 
-echo_value = False
-if "SQL_ECHO" in os.environ and os.environ["SQL_ECHO"].lower() == "yes":
-    echo_value = True
+echo_value = "SQL_ECHO" in os.environ and os.environ["SQL_ECHO"].lower() == "yes"
 
 engine = create_engine(database_url, echo=echo_value)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
