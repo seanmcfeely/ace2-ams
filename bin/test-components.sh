876ac0f5ea310866bd6d6bd25cdb5193927aac51
Holly Fox <fox.holly.13@gmail.com>
2022-06-06 11:30:39 -0400

    Second attempt

--- bin/test-components.sh
+++ bin/test-components.sh
@@ -8,7 +8,6 @@ ACE2_ENV_PATH="$HOME/.ace2.env"
 set -a
 source "$ACE2_ENV_PATH"
 export TESTING=yes
-# export CYPRESS_INSTALL_BINARY=10.0.2
 set +a
 
 # Bring up the containers (if they aren't already) in testing mode
@@ -16,6 +15,6 @@ docker-compose up -d
 
 # Run Cypress
 
-docker exec -e TZ=America/New_York ace2-ams-gui xvfb-run node_modules/.bin/cypress run --component --headed --browser chrome
+docker exec -e TZ=America/New_York ace2-ams-gui xvfb-run cypress run --component --headed --browser chrome --config-file "cypress.config.ts"
 
 bin/disable-test-mode.sh
--- bin/test-e2e.sh
+++ bin/test-e2e.sh
@@ -8,7 +8,6 @@ ACE2_ENV_PATH="$HOME/.ace2.env"
 set -a
 source "$ACE2_ENV_PATH"
 export TESTING=yes
-# export CYPRESS_INSTALL_BINARY=10.0.2
 set +a
 
 # Bring up the containers (if they aren't already) in testing mode
@@ -18,6 +17,6 @@ docker-compose -f docker-compose.yml up -d
 docker exec ace2-ams-gui bin/wait-for-gui.sh
 
 # Run Cypress
-docker exec ace2-ams-gui xvfb-run node_modules/.bin/cypress run --headed --browser chrome
+docker exec ace2-ams-gui xvfb-run node_modules/cypress/bin/cypress run --e2e --headed --browser chrome --config-file "cypress.config.ts"
 
 bin/disable-test-mode.sh
