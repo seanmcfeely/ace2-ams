5a254f4c88ed9b26c9b1ae76d71112169c4d1617
Holly Fox <fox.holly.13@gmail.com>
2022-06-06 10:40:50 -0400

    First attempt update scripts

--- bin/test-components.sh
+++ bin/test-components.sh
@@ -8,12 +8,14 @@ ACE2_ENV_PATH="$HOME/.ace2.env"
 set -a
 source "$ACE2_ENV_PATH"
 export TESTING=yes
+# export CYPRESS_INSTALL_BINARY=10.0.2
 set +a
 
 # Bring up the containers (if they aren't already) in testing mode
 docker-compose up -d
 
 # Run Cypress
-docker exec -e TZ=America/New_York ace2-ams-gui xvfb-run cypress run-ct --headed --browser chrome
+
+docker exec -e TZ=America/New_York ace2-ams-gui xvfb-run node_modules/.bin/cypress run --component --headed --browser chrome
 
 bin/disable-test-mode.sh
--- bin/test-e2e.sh
+++ bin/test-e2e.sh
@@ -8,6 +8,7 @@ ACE2_ENV_PATH="$HOME/.ace2.env"
 set -a
 source "$ACE2_ENV_PATH"
 export TESTING=yes
+# export CYPRESS_INSTALL_BINARY=10.0.2
 set +a
 
 # Bring up the containers (if they aren't already) in testing mode
@@ -17,6 +18,6 @@ docker-compose -f docker-compose.yml up -d
 docker exec ace2-ams-gui bin/wait-for-gui.sh
 
 # Run Cypress
-docker exec ace2-ams-gui xvfb-run cypress run --headed --browser chrome
+docker exec ace2-ams-gui xvfb-run node_modules/.bin/cypress run --headed --browser chrome
 
 bin/disable-test-mode.sh
