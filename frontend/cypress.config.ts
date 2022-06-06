9643bb7425180180e5648a135fa3efb79fc33c21
Holly Fox <fox.holly.13@gmail.com>
2022-06-06 12:06:56 -0400

    Move support files

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  fixturesFolder: "tests/e2e/fixtures",
-  screenshotsFolder: "tests/e2e/screenshots",
-  videosFolder: "tests/e2e/videos",
+  fixturesFolder: "cypress/fixtures",
+  screenshotsFolder: "cypress/screenshots",
+  videosFolder: "cypress/videos",
   viewportWidth: 1280,
   viewportHeight: 720,
   e2e: {
     specPattern: "tests/e2e/specs/**/*.spec.js",
-    supportFile: "tests/e2e/support/index-e2e.js",
+    supportFile: "cypress/support/index-e2e.js",
     baseUrl: "http://ace2-ams:8080",
   },
   component: {
     specPattern: "tests/component/src/**/*.spec.ts",
-    supportFile: "tests/e2e/support/index-ct.js",
+    supportFile: "cypress/support/index-ct.js",
     devServer: {
       framework: "vue",
       bundler: "vite",
