9d560d72e0989bcf4166b4f8dc05e7bb5b736369
brandon-bullen <brandon.bullen@53.com>
2022-06-09 16:35:32 -0400

    incrementing tf module

--- modules/file_type/terraform/main.tf
+++ modules/file_type/terraform/main.tf
@@ -19,7 +19,7 @@ provider "aws" {
 
 module "filetype-idr-analysis-module" {
   source  = "app.terraform.io/FifthThird/idr-analysis-module/aws"
-  version = "1.0.4"
+  version = "1.0.5"
   #Vars
   environment              = var.environment
   analysis_module_name     = var.analysis_module_name
