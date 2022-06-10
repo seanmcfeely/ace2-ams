c99a0ccb5e418b682a702f9d94ec2b0dabc6a5a5
Holly Fox <fox.holly.13@gmail.com>
2022-06-09 23:04:59 -0400

    Update test

--- gui_api/app/tests/api/observable/test_create.py
+++ gui_api/app/tests/api/observable/test_create.py
@@ -3,13 +3,13 @@ from uuid import uuid4
 
 def test_create_observable(client_valid_access_token, requests_mock):
     requests_mock.post(
-        f"http://db-api/api/observable/",
-        json={
+        "http://db-api/api/observable/",
+        json=[{
             "uuid": "uuid1",
-        },
+        }]
     )
 
-    client_valid_access_token.post(f"/api/observable/", json={"type": "type", "value": "value"})
+    client_valid_access_token.post("/api/observable/", json=[{"type": "type", "value": "value"}])
 
     assert (len(requests_mock.request_history)) == 2
     assert requests_mock.request_history[1].method == "POST"
