fcd29cb9c289d3b412390e37314c8441710949de
Holly Fox <fox.holly.13@gmail.com>
2022-06-15 09:42:47 -0400

    Add event filtering "none" owner support

--- db_api/app/db/crud/event.py
+++ db_api/app/db/crud/event.py
@@ -230,7 +230,11 @@ def build_read_all_query(
         query = _join_as_subquery(query, observable_value_query)
 
     if owner:
-        owner_query = select(Event).join(User, onclause=Event.owner_uuid == User.uuid).where(User.username == owner)
+        owner_query = select(Event)
+        if owner.lower() == "none":
+            owner_query = owner_query.where(Submission.owner_uuid == None)  # pylint: disable=singleton-comparison
+        else:
+            owner_query = owner_query.join(User, onclause=Event.owner_uuid == User.uuid).where(User.username == owner)
         query = _join_as_subquery(query, owner_query)
 
     if prevention_tools:
