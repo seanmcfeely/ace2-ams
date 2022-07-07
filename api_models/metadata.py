4c09c00b5f08002dde1f1adeb531fa9280e8f270
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-07-07 15:22:20 -0400

    Adds new Pydantic models that are compatible with mypy

--- api_models/metadata.py
+++ api_models/metadata.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field, UUID4
 from uuid import uuid4
 
-from api_models import type_str
+from api_models import TypeStr
 
 
 class MetadataBase(BaseModel):
@@ -16,7 +16,7 @@ class MetadataCreate(MetadataBase):
 
 class MetadataRead(MetadataBase):
 
-    metadata_type: type_str = Field(description="The type of the metadata")
+    metadata_type: TypeStr = Field(description="The type of the metadata")
 
     uuid: UUID4 = Field(description="The UUID of the metadata")
 
