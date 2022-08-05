8620c6305e6a23a1405a6e2d2aa1f3232b01cb43
Matthew Wilson <22526682+automationator@users.noreply.github.com>
2022-08-04 20:21:17 -0400

    Debugging

--- frontend/test-e2e.sh
+++ frontend/test-e2e.sh
@@ -12,9 +12,4 @@ docker compose -f $COMPOSE_FILE up -d
 docker exec ace2-frontend bin/wait-for-gui.sh
 
 # Run Cypress
-docker exec -e VITE_TESTING_MODE=yes ace2-frontend curl http://ace2-ams:8080/login
-docker exec -e VITE_TESTING_MODE=yes ace2-frontend echo "db_api"
-docker exec -e VITE_TESTING_MODE=yes ace2-frontend curl http://db-api/api/ping
-docker exec -e VITE_TESTING_MODE=yes ace2-frontend echo "gui_api"
-docker exec -e VITE_TESTING_MODE=yes ace2-frontend curl http://gui-api/api/ping
-#docker exec -e VITE_TESTING_MODE=yes ace2-frontend xvfb-run cypress run --e2e --headless --browser chrome --config-file "cypress.config.ts" --config video=false,screenshotOnRunFailure=false
+docker exec -e VITE_TESTING_MODE=yes ace2-frontend xvfb-run cypress run --e2e --headless --browser chrome --config-file "cypress.config.ts" --config video=false,screenshotOnRunFailure=false
